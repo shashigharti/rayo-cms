98b534a8c63018ca94bd1410817e9288f8a646dd
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-07-22 09:25:34 +0545

    updated readme removed extra routes

--- routes/api.php
+++ routes/api.php
@@ -20,65 +20,6 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::post('details', 'API\UserController@details');
 });
 
-// Get All Profile Reports
-Route::get('/{local_government_id}/profile',[
-    'as' => 'profile-reports.getReport',
-    'uses' => 'ProfileReportsController@getProfileReport'
-]);
-
-Route::get('/map-details/{type}/{local_government}',[
-    'as' => 'map.getDetails',
-    'uses' => 'SurveyController@getDetails'
-]);
-
-Route::get('/family-config',[
-    'as' => 'family-details.getFamilyDetailsConfig',
-    'uses' => 'SurveyController@getFamilyDetailsConfig'
-]);
-
-// Metrics API
-Route::prefix('metrics')->group(function() {
-    Route::get('surveyors',[
-        'as' => 'metrics.total-surveys',
-        'uses' => 'SurveyController@totalSurveyors'
-    ]);
-
-    Route::get('total-users',[
-        'as' => 'metrics.total-users',
-        'uses' => 'SurveyController@getTotalUsers'
-    ]);
-
-    Route::get('weekly-active-users',[
-        'as' => 'metrics.weekly-active-users',
-        'uses' => 'SurveyController@getWeeklyActiveUsers'
-    ]);
-
-    Route::get('weekly-submissions',[
-        'as' => 'metrics.weekly-submissions',
-        'uses' => 'SurveyController@getDailyWeeklySubmissions'
-    ]);
-
-    Route::get('total-population',[
-        'as' => 'metrics.total-population',
-        'uses' => 'SurveyController@getTotalPopulation'
-    ]);
-
-    Route::get('household-count',[
-        'as' => 'metrics.household-count',
-        'uses' => 'SurveyController@getHouseholdCount'
-    ]);
-
-    Route::get('male-female',[
-        'as' => 'metrics.male-female-count',
-        'uses' => 'SurveyController@getTotalMaleFemale'
-    ]);
-});
-
-Route::get('/local-governments',[
-    'as' => 'profile.local-governments',
-    'uses' => 'ProfileReportsController@getAllLocalGovernments'
-]);
-
 
 
 
