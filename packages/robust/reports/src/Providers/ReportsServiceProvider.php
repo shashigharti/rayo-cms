d294ae129d450ad62d613e56056a5eaae346d867
Kundan Karna <mysoulsource.np@gmail.com>
2019-10-18 11:05:47 +0545

    added provieder for reports

---
+++ packages/robust/reports/src/Providers/ReportsServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+namespace Robust\Reports\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Webwizo\Shortcodes\Facades\Shortcode;
+
+/**
+ * Class PagesServiceProvider
+ * @package Robust\Pages\Providers
+ */
+class ReportsServiceProvider extends ServiceProvider
+{
+    /**
+     *
+     */
+    public function register()
+    {
+
+    }
+}
