1898236aa1b12917d3055ce33240a867a65117c6
Kundan Karna <mysoulsource.np@gmail.com>
2019-10-18 15:52:48 +0545

    fixed grids api

--- packages/robust/landmarks/routes/api/grids.php
+++ packages/robust/landmarks/routes/api/grids.php
@@ -2,6 +2,6 @@
 Route::group(['prefix' => config('core.frw.api'), 'as' => 'api.', 'group' => 'Grids API'], function () {
     Route::get('grids/all', [
         'as' => 'api.grids.all',
-        'uses' => '\Robust\Landmarks\Controllers\Admin\GridController@getAll'
+        'uses' => '\Robust\Landmarks\Controllers\Api\GridController@getAll'
     ]);
 });
