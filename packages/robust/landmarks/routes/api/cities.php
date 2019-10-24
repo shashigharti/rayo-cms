9aa011165a0169f94cebd85e0fd0b275acec7df5
Kundan Karna <mysoulsource.np@gmail.com>
2019-10-24 10:32:57 +0545

    confict resolved

--- packages/robust/landmarks/routes/api/cities.php
+++ packages/robust/landmarks/routes/api/cities.php
@@ -1,11 +1,4 @@
 <?php
 Route::group(['prefix' => config('core.frw.api'), 'as' => 'api.', 'group' => 'City API'], function () {
-<<<<<<< HEAD
     Route::apiResource('city', '\Robust\Landmarks\Controllers\Admin\CityApiController');
-=======
-    Route::get('city/all', [
-        'as' => 'api.cities.all',
-        'uses' => '\Robust\Landmarks\Controllers\Api\CityController@getAll'
-    ]);
->>>>>>> feature/mls
 });
