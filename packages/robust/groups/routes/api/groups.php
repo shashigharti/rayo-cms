2fddc1377a9c8438f5d25f08844935fcdda96eb3
Kundan Karna <mysoulsource.np@gmail.com>
2019-10-25 09:35:20 +0545

    fixed settings route

--- packages/robust/groups/routes/api/groups.php
+++ packages/robust/groups/routes/api/groups.php
@@ -2,21 +2,21 @@
 Route::group(['prefix' => config('core.frw.api'), 'as' => 'api.', 'group' => 'API Groups'], function () {
     Route::get('groups/all', [
         'as' => 'api.groups.all',
-        'uses' => '\Robust\Groups\Controllers\Api\GroupsApiController@getAll'
+        'uses' => '\Robust\Groups\Controllers\Api\GroupsController@getAll'
     ]);
 
     Route::post('groups/store', [
         'as' => 'api.groups.store',
-        'uses' => '\Robust\Groups\Controllers\Api\GroupsApiController@store'
+        'uses' => '\Robust\Groups\Controllers\Api\GroupsController@store'
     ]);
 
     Route::put('groups/update/{id}', [
         'as' => 'api.groups.update',
-        'uses' => '\Robust\Groups\Controllers\Api\GroupsApiController@update'
+        'uses' => '\Robust\Groups\Controllers\Api\GroupsController@update'
     ]);
 
     Route::delete('groups/delete/{id}', [
         'as' => 'api.groups.destroy',
-        'uses' => '\Robust\Groups\Controllers\Api\GroupsApiController@destroy'
+        'uses' => '\Robust\Groups\Controllers\Api\GroupsController@destroy'
     ]);
 });
