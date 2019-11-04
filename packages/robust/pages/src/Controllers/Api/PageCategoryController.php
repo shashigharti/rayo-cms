cb48f55f7f685ffc76fc549f5dfce0cf6b3c503e
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-04 09:19:56 +0545

    refactored page api

--- packages/robust/pages/src/Controllers/Api/PageCategoryController.php
+++ packages/robust/pages/src/Controllers/Api/PageCategoryController.php
@@ -3,9 +3,8 @@
 namespace Robust\Pages\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Robust\Pages\Models\Category;
-use Robust\Pages\Resources\Category as CategoryResource;
+use Robust\Core\Controllers\Admin\Traits\ApiTrait;
+use Robust\Pages\Repositories\Admin\CategoryRepository;
 
 
 /**
@@ -14,70 +13,22 @@ use Robust\Pages\Resources\Category as CategoryResource;
  */
 class PageCategoryController extends Controller
 {
+    use ApiTrait;
+    /**
+     * @var CategoryRepository
+     */
+    /**
+     * @var CategoryRepository|string
+     */
+    protected $model,$resource;
 
     /**
-     * @param \Robust\Pages\Models\Category $category
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * PageCategoryController constructor.
+     * @param CategoryRepository $model
      */
-    public function getAll(Category $category)
+    public function __construct(CategoryRepository $model)
     {
-        return CategoryResource::collection($category->all());
-    }
-
-
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @param \Robust\Pages\Models\Category $category
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store(Request $request, Category $category)
-    {
-        $data = $request->all();
-        $rules = [
-            "name" => "required",
-            "slug" => "required|unique:pages_categories",
-            "description" => "required"
-        ];
-        $request->validate($rules);
-        $category->create($data);
-
-        return response()->json(['message' => 'Success']);
-    }
-
-
-    /**
-     * @param $id
-     * @param \Illuminate\Http\Request $request
-     * @param \Robust\Pages\Models\Category $category
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function update($id, Request $request, Category $category)
-    {
-        $data = $request->all();
-        $updated = $category->find($id)->update($data);
-
-        if ($updated) {
-            return response()->json(['message' => 'Success']);
-        } else {
-            return response()->json(['message' => 'Failed']);
-        }
-    }
-
-
-    /**
-     * @param $id
-     * @param \Robust\Pages\Models\Category $category
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function destroy($id, Category $category)
-    {
-        $deleted = $category->find($id)->delete();
-
-        if ($deleted) {
-            return response()->json(['message' => 'Success']);
-        } else {
-            return response()->json(['message' => 'Failed']);
-        }
-
+        $this->model = $model;
+        $this->resource = 'Robust\Pages\Resources\Category';
     }
 }
