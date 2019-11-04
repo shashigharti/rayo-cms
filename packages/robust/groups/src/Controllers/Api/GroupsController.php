fc306ee23a10681853ce39df5550122a0b07b92d
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-04 08:22:32 +0545

    refactored groups api

--- packages/robust/groups/src/Controllers/Api/GroupsController.php
+++ packages/robust/groups/src/Controllers/Api/GroupsController.php
@@ -3,10 +3,8 @@
 namespace Robust\Groups\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Exception;
-use Illuminate\Http\Request;
-use Robust\Groups\Model\CoreGroup;
-use Robust\Groups\Resources\CoreGroup as CoreGroupResource;
+use Robust\Core\Controllers\Admin\Traits\ApiTrait;
+use Robust\Groups\Repositories\Admin\GroupRepository;
 
 
 /**
@@ -15,66 +13,23 @@ use Robust\Groups\Resources\CoreGroup as CoreGroupResource;
  */
 class GroupsController extends Controller
 {
+    use ApiTrait;
     /**
-     * @param \Robust\Groups\Model\CoreGroup $coreGroup
-     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @var GroupRepository
      */
-    public function getAll(CoreGroup $coreGroup)
-    {
-        return CoreGroupResource::collection($coreGroup->all());
-    }
-
+    protected $model;
     /**
-     * @param \Illuminate\Http\Request $request
-     * @param \Robust\Groups\Model\CoreGroup $coreGroup
-     * @return \Illuminate\Http\JsonResponse
+     * @var string
      */
-    public function store(Request $request, CoreGroup $coreGroup)
-    {
-        try {
-            $coreGroup->create($request->all());
-        } catch (Exception $e) {
-            if ($e->getCode() === "23000") {
-                return response()->json([
-                    'message' => 'Duplicate entry. Please use a different name / color.',
-                    'class' => 'red darken-2'
-                ]);
-            }
-        }
-
-        return response()->json(['message' => 'Successfully Saved.', 'class' => '']);
-    }
+    protected $resource;
 
     /**
-     * @param $id
-     * @param \Illuminate\Http\Request $request
-     * @param \Robust\Groups\Model\CoreGroup $coreGroup
-     * @return \Illuminate\Http\JsonResponse
+     * GroupsController constructor.
+     * @param GroupRepository $model
      */
-    public function update($id, Request $request, CoreGroup $coreGroup)
+    public function __construct(GroupRepository $model)
     {
-        $dataToUpdate = $request->all();
-        $coreGroup->find($id)->update($dataToUpdate);
-        return response()->json(['message' => 'Successfully Updated']);
-    }
-
-    /**
-     * @param $id
-     * @param \Robust\Groups\Model\CoreGroup $coreGroup
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function destroy($id, CoreGroup $coreGroup)
-    {
-        try {
-            $coreGroup->find($id)->delete();
-        } catch (Exception $e) {
-            return response()->json([
-                'message' => 'Deleted failed',
-                'class' => 'red darken-1',
-                'error' => $e->getMessage()
-            ]);
-        }
-
-        return response()->json(['message' => 'Successfully deleted.', 'class' => '']);
+        $this->model = $model;
+        $this->resource = 'Robust\Groups\Resources\CoreGroup';
     }
 }
