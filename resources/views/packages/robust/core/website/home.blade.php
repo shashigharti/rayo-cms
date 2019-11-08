e7e6c88d4a55e8687cfa201e831f7eeddd2c3146
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-08 14:01:33 +0545

    removed about us

--- resources/views/packages/robust/core/website/home.blade.php
+++ resources/views/packages/robust/core/website/home.blade.php
@@ -5,7 +5,6 @@
     @include(Site::templateResolver('core::website.partials.properties'))
     @include(Site::templateResolver('core::website.partials.common.advertisement'))
     @include(Site::templateResolver('core::website.partials.common.cta'))
-    @include(Site::templateResolver('core::website.partials.common.about-us'))
     @include(Site::templateResolver('core::website.partials.common.footer'))
 @endsection
 
