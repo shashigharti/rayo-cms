bd27232f96c65190206ce18c195dc494dc1429b7
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-11 11:20:34 +0545

    mls image modified

--- packages/robust/real-estate/src/Console/Commands/MlsPullImages.php
+++ packages/robust/real-estate/src/Console/Commands/MlsPullImages.php
@@ -3,7 +3,10 @@
 
 namespace Robust\RealEstate\Console\Commands;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Robust\RealEstate\Helpers\MlsPullHelper;
+use Robust\RealEstate\Models\Listing;
+use Robust\RealEstate\Models\ListingImages;
 use Robust\RealEstate\Models\MlsUser;
 
 class MlsPullImages extends Command
@@ -32,7 +35,58 @@ class MlsPullImages extends Command
             $config->setOption('use_post_method', true);
             $rets = new \PHRETS\Session($config);
             $connect = $rets->login();
-            $helper->getImages($rets);
+            $image_type = 'Photo';
+            $format = 1;
+            $listingArrChunked = Listing::query()
+                ->leftJoin('listing_images', function ($join) {
+                    $join->on('listing_images.listing_id', '=', 'listings.uid');
+                })
+                ->whereNull('listing_images.id')
+                ->select(['listings.id', 'listings.uid', 'listings.status'])
+                ->orderBy('input_date', 'DESC')
+                ->get()
+                ->chunk(5);
+            $bar = $this->output->createProgressBar(count($listingArrChunked));
+            foreach ($listingArrChunked as $listingArr) {
+                $ui_id_arr = $listingArr->pluck('uid')->toArray();
+                $listing_status = $listingArr->pluck('status','uid')->toArray();
+                $query = implode(',', $ui_id_arr);
+
+                $objects = $rets->GetObject('Property', $image_type, $query, '*', $format);
+                foreach ($objects as $object)
+                {
+                    try{
+                        $listing_url = '/media/images/image-not-found.png';
+                        if($format = 1)
+                        {
+                            $listing_url = $object->getLocation();
+                        } else {
+                            $encoded_image = $object->getContent();
+                            $file_path = '/media/images/' . $object->getContentId() . '-' .$object->getObjectId().'.jpg';
+                            $absolute_path = storage_path() . $file_path;
+                            file_put_contents($absolute_path,$encoded_image);
+                            $listing_url = $file_path;
+                        }
+                        $data = [
+                            'listing_id' => $object->getContentId(),
+                            'image_id' => $object->getObjectId() ,
+                            'type' => $object->getContentType(),
+                            'listing_url' => $listing_url
+                        ];
+                        Log::info($data);
+                        ListingImages::updateOrCreate(['listing_id' => $data['listing_id'],'image_id'=>$data['image_id']],$data);
+                        if($listing_status[$data['listing_id']] != 'Active'){
+                            continue;
+                        }
+                        $this->info('ID : '.$data['listing_id'] . ' || '.'Status : ' . $listing_status[$data['listing_id']]);
+                    }
+                    catch(\Exception $er){
+                        Log::info($er);
+                    }
+                }
+                $bar->advance();
+            }
+            $bar->finish();
         }
 
     }
