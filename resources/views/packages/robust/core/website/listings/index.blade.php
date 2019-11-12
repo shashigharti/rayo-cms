e95a747e06a9a85fde5ff770abd8b9c1253a861d
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-12 14:44:53 +0545

    fixed blade

--- resources/views/packages/robust/core/website/listings/index.blade.php
+++ resources/views/packages/robust/core/website/listings/index.blade.php
@@ -1,6 +1,6 @@
 @extends(Site::templateResolver('core::website.layouts.default'))
 @section('content')
-    @include(Site::templateResolver('core::website.listings.partials..header'))
+    @include(Site::templateResolver('core::website.listings.partials.header'))
     @include(Site::templateResolver('core::website.listings.partials.main'))
     @include(Site::templateResolver('core::website.listings.partials.footer'))
 @endsection
