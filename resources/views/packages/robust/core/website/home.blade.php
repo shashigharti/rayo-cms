16a37a997ea8ddf72b2800e0610309034027abb4
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-11-13 23:10:50 +0545

    Removed unused file

--- resources/views/packages/robust/core/website/home.blade.php
+++ resources/views/packages/robust/core/website/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include(Site::templateResolver('core::website.partials.common.header'))
     @include(Site::templateResolver('core::website.partials.properties'))
-    @include(Site::templateResolver('core::website.partials.common.advertisement'))
+    {{-- @include(Site::templateResolver('core::website.partials.common.advertisement')) --}}
     @include(Site::templateResolver('core::website.partials.common.cta'))
     @include(Site::templateResolver('core::website.partials.common.footer'))
 @endsection
