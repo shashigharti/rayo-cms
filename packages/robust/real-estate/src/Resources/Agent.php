2897bdcc9f6bae0369f56a09a6bc18e8523b43ad
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-17 16:35:24 +0545

    added resource fields for agent

--- packages/robust/real-estate/src/Resources/Agent.php
+++ packages/robust/real-estate/src/Resources/Agent.php
@@ -21,6 +21,10 @@ class Agent extends JsonResource
             'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
+            'user_name' => $this->user_name,
+            'email' => $this->email,
+            'contact' => $this->contact,
+            'address' => $this->address
         ];
     }
 }
