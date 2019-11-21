8979fc152eac2ce01aaac97ff34a656cfe67c8e1
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-11-21 14:45:37 +0545

    Commented to refactor

--- resources/views/packages/robust/core/website/banners/partials/full-screen-ad.blade.php
+++ resources/views/packages/robust/core/website/banners/partials/full-screen-ad.blade.php
@@ -1,5 +1,5 @@
 <section class="adv--single">
-    <img src={{$buy_now[0]->media->file}}>
+    {{-- <img src={{$buy_now[0]->media->file}}> --}}
     <div class="adv-single-text">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
         <a href="#" class="buy-now-btn">Buy Now</a>
