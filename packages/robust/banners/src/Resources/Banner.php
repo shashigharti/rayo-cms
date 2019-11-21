c0c649c2a103e2a3a5422afd9519421a2a26e59e
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-21 11:45:05 +0545

    check index for banner properties

--- packages/robust/banners/src/Resources/Banner.php
+++ packages/robust/banners/src/Resources/Banner.php
@@ -18,19 +18,19 @@ class Banner extends JsonResource
            'id' => $this->id,
            'name' => $this->name,
            'slug' => $this->slug,
-            'header' => $blocks['header'],
-            'area_types' => $blocks['area_types'],
-            'sub_areas' => $blocks['sub_areas'],
-            'property_ids' => $blocks['property_ids'],
-            'button_text' => $blocks['button_text'],
-            'button_url' => $blocks['button_url'],
+            'header' => isset($blocks['header'])?  $blocks['header'] : '',
+            'area_types' =>isset($blocks['area_types']) ? $blocks['area_types'] : [],
+            'sub_areas' =>isset($blocks['sub_areas']) ? $blocks['sub_areas'] : [],
+            'property_ids' =>isset($blocks['property_ids']) ? $blocks['property_ids'] : [],
+            'button_text' => isset($blocks['button_text']) ? $blocks['button_text'] : '',
+            'button_url' =>isset($blocks['button_url']) ? $blocks['button_url'] : '',
             'banner_template' => $this->template,
             'status' => $this->status,
             'order' => $this->order,
-            'prices' => $blocks['prices'],
-            'locations' => $blocks['locations'],
-            'content' => $blocks['content'],
-            'images' => $blocks['images']
+            'prices' => isset($blocks['locations']) ? $blocks['locations'] : [],
+            'locations' => isset($blocks['locations']) ? $blocks['locations'] : [],
+            'content' => isset($blocks['content']) ? $blocks['content'] : '',
+            'images' => isset($blocks['images'])  ? $blocks['images'] : []
         ];
     }
 }
