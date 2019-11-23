6fc52da971ff9346af18617b60d05acab931da6d
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-11-23 13:32:22 +0545

    REmoved all the extra code from real-estate module

--- packages/robust/real-estate/src/Providers/RealEstateServiceProvider.php
+++ packages/robust/real-estate/src/Providers/RealEstateServiceProvider.php
@@ -23,14 +23,6 @@ class RealEstateServiceProvider extends ServiceProvider
     public function register_includes()
     {
         $this->mergeConfigFrom(__DIR__ . '/../../config/permissions.php', 'real-estate.permissions');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/listings.php', 'mls.listings');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/glenn.php', 'mls.glenn');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/columns.php', 'mls.columns');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/query_filter.php', 'mls.query');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/tampa.php', 'mls.tampa');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/santa.php', 'mls.santa');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/iowa.php', 'mls.iowa');
-        $this->mergeConfigFrom(__DIR__ . '/../../config/panama.php', 'mls.panama');
         $this->mergeConfigFrom(__DIR__ . '/../../config/advance-search.php', 'mls.search');
         $this->commands($this->commands);
     }
