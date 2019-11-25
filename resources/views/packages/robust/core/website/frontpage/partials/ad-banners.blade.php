c2a1536be43a680a3625e97522318f1b30d55c33
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-25 14:14:24 +0545

    fixed sliders multiple showing

--- resources/views/packages/robust/core/website/frontpage/partials/ad-banners.blade.php
+++ resources/views/packages/robust/core/website/frontpage/partials/ad-banners.blade.php
@@ -1,6 +1,6 @@
-@set('adBanners', $banner_helper->getBannersNotInType(['banner-slider','single-col-block']))
+@set('adBanners', $banner_helper->getBannersNotInType(['banner-slider','single-col-block','slider']))
 @include(Site::templateResolver("core::website.banners.partials.single-col-block"))
 @foreach($adBanners as $adBanner)
     @include(Site::templateResolver("core::website.banners.partials.{$adBanner->template}"))
 @endforeach
-
+@include(Site::templateResolver("core::website.banners.partials.slider"))
