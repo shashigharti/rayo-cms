66fdac8177158af4ac45384c738c155d18e2ddd3
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-25 16:58:28 +0545

    added migration for market reports table

---
+++ packages/robust/real-estate/database/migrations/2019_11_25_110848_create_real_estate_market_reports_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRealEstateMarketReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_estate_market_reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('reportable_id');
+            $table->string('reportable_type');
+            $table->string('slug');
+            $table->string('name');
+            $table->integer('total_listings')->nullable();
+            $table->integer('total_listings_active')->nullable();
+            $table->integer('total_listings_sold')->nullable();
+            $table->integer('total_listings_sold_past_year')->nullable();
+            $table->integer('total_listings_sold_this_year')->nullable();
+            $table->integer('median_price_active')->nullable();
+            $table->integer('median_price_sold')->nullable();
+            $table->integer('median_price_sold_past_year')->nullable();
+            $table->integer('median_price_sold_this_year')->nullable();
+            $table->integer('average_price_active')->nullable();
+            $table->integer('average_price_sold')->nullable();
+            $table->integer('average_price_sold_past_year')->nullable();
+            $table->integer('average_price_sold_this_year')->nullable();
+            $table->integer('average_dos')->nullable();
+            $table->integer('average_dos_past_year')->nullable();
+            $table->integer('average_dos_this_year')->nullable();
+            $table->integer('median_dos')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_estate_market_reports');
+    }
+}
