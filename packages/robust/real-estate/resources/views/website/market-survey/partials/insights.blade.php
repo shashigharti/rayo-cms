1fde84b222719788ac078f143d91a41a45e04b84
Bhumika <ganeshbhumika876@gmail.com>
2019-11-27 21:13:23 +0545

    insights-photo-block

--- packages/robust/real-estate/resources/views/website/market-survey/partials/insights.blade.php
+++ packages/robust/real-estate/resources/views/website/market-survey/partials/insights.blade.php
@@ -2,7 +2,7 @@
 	<div class="market-survey__insights--trends region-content-section border-bottom">
 		<h2 class="title primary-heading text-left">Home Prices in <!-- -->San Diego</h2>
 		<h3 class="secondary-heading">Average over the last month</h3>
-		<div class="DetailsComponent detail--component horizontal primary">
+		<div class="detail--component horizontal primary">
 			<div class="detail">
 				<div class="row">
 					<div class="value">
@@ -44,6 +44,61 @@
 			<div class="divider"></div>
 			<div class="detail">
 				<div class="label">Total Homes Sold</div><div class="value"><span>1060</span></div>
+			</div>
+		</div>
+		<div class="market-survey__insights--CTA region-content-section border-bottom">
+			<h2 class="title primary-heading">Make a Winning Offer in San Diego with a Redfin Agent</h2>
+			<div class="cta-container">
+				<a href="#" class="agents-image-container">
+					<img class="agents-image" src="https://ssl.cdn-redfin.com/v234.2.2/images/agents/two_agents.jpg" alt="Two Redfin Agents">
+				</a>
+				<div class="cta-description">
+					<p>Spend an hour learning about how to win your perfect home.</p>
+				</div>
+				<a href="#" class="button Button button tertiary cta-button" tabindex="0">
+					<span>Meet with a Redfin Agent</span>
+				</a>
+			</div>
+		</div>
+		<div class="neighborhood-photos-section region-content-section" id="neighborhood-photos-section">
+			<h2 class="title primary-heading">Photos of San Diego</h2>
+			<div class="neighborhood-photos-container count-6">
+				<div class="place">
+					<div class="photo-container">
+						<img src="img/block.jpg">
+					</div>
+					<p class="place-name">Garden</p>
+				</div>
+				<div class="place">
+					<div class="photo-container">
+						<img src="img/block2.jpg">
+					</div>
+					<p class="place-name">Dwight St. &amp; Lantana Dr.</p>
+				</div>
+				<div class="place">
+					<div class="photo-container">
+						<img src="img/block3.jpg">
+					</div>
+					<p class="place-name">Carlton St &amp; Fennel on St.</p>
+				</div>
+				<div class="place">
+					<div class="photo-container">
+						<img src="img/block.jpg">
+					</div>
+					<p class="place-name">Mountain View Community Center</p>
+				</div>
+				<div class="place">
+					<div class="photo-container">
+						<img src="img/block2.jpg">
+					</div>
+					<p class="place-name">La Jolla Cove</p>
+				</div>
+				<div class="place">
+					<div class="photo-container">
+						<img src="img/block3.jpg">
+					</div>
+					<p class="place-name">Lahaina Beach House</p>
+				</div>
 			</div>
 		</div>
 	</div>
