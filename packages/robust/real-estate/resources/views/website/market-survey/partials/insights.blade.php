3bd7d58bdfb81a3a23cfae8417f13280b8ec1eda
Bhumika <ganeshbhumika876@gmail.com>
2019-11-27 21:00:20 +0545

    insighst-trends-design

--- packages/robust/real-estate/resources/views/website/market-survey/partials/insights.blade.php
+++ packages/robust/real-estate/resources/views/website/market-survey/partials/insights.blade.php
@@ -1,2 +1,50 @@
-<div class="row">
+<div id="market-survey__insights--content" class="row">
+	<div class="market-survey__insights--trends region-content-section border-bottom">
+		<h2 class="title primary-heading text-left">Home Prices in <!-- -->San Diego</h2>
+		<h3 class="secondary-heading">Average over the last month</h3>
+		<div class="DetailsComponent detail--component horizontal primary">
+			<div class="detail">
+				<div class="row">
+					<div class="value">
+						<span>$616K</span>
+					</div>
+					<div class="label">Sale Price</div>
+				</div>
+				<div class="sub-label">
+					<span>+12.6%<br> since last year</span>
+				</div>
+			</div>
+			<div class="detail saleSqft">
+				<div class="row">
+					<div class="value">
+						<span>$441</span>
+					</div>
+					<div class="label">Sale $/Sq. Ft.</div>
+				</div>
+				<div class="sub-label">
+					<span>+10.0%<br> since last year</span>
+				</div>
+			</div>
+		</div>
+		<div class="detail--component horizontal">
+			<div class="detail">
+				<div class="label">Under List Price</div>
+				<div class="value">1.2%</div>
+			</div>
+			<div class="divider"></div>
+			<div class="detail">
+				<div class="label">Days on Market</div>
+				<div class="value"><span>19</span></div>
+			</div>
+			<div class="divider"></div>
+			<div class="detail">
+				<div class="label">Down Payment</div>
+				<div class="value"><span>28.2%</span></div>
+			</div>
+			<div class="divider"></div>
+			<div class="detail">
+				<div class="label">Total Homes Sold</div><div class="value"><span>1060</span></div>
+			</div>
+		</div>
+	</div>
 </div>   
