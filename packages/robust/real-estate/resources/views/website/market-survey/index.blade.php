6402b7e1e054f175c9528a433ad56991732ff238
Bhumika <ganeshbhumika876@gmail.com>
2019-11-27 19:43:08 +0545

    market-listing-design

--- packages/robust/real-estate/resources/views/website/market-survey/index.blade.php
+++ packages/robust/real-estate/resources/views/website/market-survey/index.blade.php
@@ -6,29 +6,33 @@
 @section('body_section')
     <section class="market-survey main-content map-section">
         <div class="row">
-            <div id="market-survey__left-container" class="market-survey__left-container map col m8" >
-                
+           <div class="col s7">
+                <div id="market-survey__left-container" class="market-survey__left-container map" >
+                    
+                </div>
             </div>
-            <div class="market-survey__right-container col m4">
+            <div class="col s5">
+                <div class="market-survey__right-container">
                 <div class="market-survey__right-container--tabs">
-                    <h1>Alaska Real State</h1>
-                    <div class="col s12">
+                    <div class="market-survey__right-container--header">
+                        <h1>Alaska Real State</h1>
                         <ul class="tabs">
-                            <li class="tab col s3"><a href="#listings">Listings</a></li>
-                            <li class="tab col s3"><a class="active" href="#insights">Market Insights</a></li>
+                            <li class="tab"><a href="#listings">Listings</a></li>
+                            <li class="tab"><a class="active" href="#insights">Market Insights</a></li>
                         </ul>
                     </div>
-                    <div id="listings" class="col s12">
+                    <div id="listings" class="market-survey__listings">
                         @include(Site::templateResolver('real-estate::website.market-survey.partials.listings'))
                     </div>
                     <div id="insights" class="col s12">
                         @include(Site::templateResolver('real-estate::website.market-survey.partials.insights'))
                     </div>
                 </div>
+                </div>
             </div>
         </div>
     </section>
-    <script>
+    <!-- <script>
         function initMap() {
             var map = new google.maps.Map(document.getElementById('market-survey__left-container'), {
                 center: {lat: -34.397, lng: 150.644},
@@ -54,9 +58,10 @@
             });
             drawingManager.setMap(map);
         }
-    </script>
+    </script> -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVMGPU0xbiE-XtO-U61AltLGW05KKF0cY&libraries=drawing&callback=initMap"
             async defer></script>
 @endsection
+
 
 
