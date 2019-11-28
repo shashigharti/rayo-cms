950cd2bfaa324e0ae6cd846699ca9a1f8ed678cf
Kundan Karna <mysoulsource.np@gmail.com>
2019-11-28 16:09:38 +0545

    banners table migration

--- packages/robust/banners/database/migrations/2016_12_14_025046_create_banners_table.php
+++ packages/robust/banners/database/migrations/2016_12_14_025046_create_banners_table.php
@@ -17,6 +17,10 @@ class CreateBannersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('slug');
+            $table->string('template',20)->nullable();
+            $table->text('properties')->nullable();
+            $table->integer('order')->nullable();
+            $table->boolean('status')->nullable();
             $table->timestamps();
         });
     }
