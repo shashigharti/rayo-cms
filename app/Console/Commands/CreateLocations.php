624cb080c1106e0c2716af0038cb22acc379c23b
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-12-03 07:34:53 +0545

    Completed Create Locations

--- app/Console/Commands/CreateLocations.php
+++ app/Console/Commands/CreateLocations.php
@@ -2,7 +2,17 @@
 namespace App\Console\Commands;
 
 use Robust\RealEstate\Models\City;
+use Robust\RealEstate\Models\SchoolDistrict;
+use Robust\RealEstate\Models\County;
 use Robust\RealEstate\Models\Listing;
+use Robust\RealEstate\Models\Zip;
+use Robust\RealEstate\Models\Area;
+use Robust\RealEstate\Models\HighSchool;
+use Robust\RealEstate\Models\ElemSchool;
+use Robust\RealEstate\Models\MiddleSchool;
+use Robust\RealEstate\Models\Grid;
+use Robust\RealEstate\Models\Subdivision;
+
 use Illuminate\Console\Command;
 
 /**
@@ -29,18 +39,18 @@ class CreateLocations extends Command
      */
     public function handle()
     {        
-        // Read this data from config
+        // Read from config : settings
         $locations = [
-            'city' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'district' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'county' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'zip' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'area' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'high_school' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'elem_school' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'middle_school' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'],
-            'grid' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name'] ,
-            'subdivision' => ['table_name'=>'real_estate_cities', 'field_to_join' => 'name']
+            'city' => ['table_name'=>'real_estate_cities'],
+            // 'district' => ['table_name'=>'real_estate_school_districts'],
+            // 'county' => ['table_name'=>'real_estate_counties'],
+            // 'zip' => ['table_name'=>'real_estate_zips'],
+            // 'area' => ['table_name'=>'real_estate_areas'],
+            // 'high_school' => ['table_name'=>'real_estate_high_schools'],
+            // 'elem_school' => ['table_name'=>'real_estate_elem_schools'],
+            // 'middle_school' => ['table_name'=>'real_estate_middle_schools'],
+            // 'grid' => ['table_name'=>'real_estate_grids'] ,
+            // 'subdivision' => ['table_name'=>'real_estate_subdivisions']
         ];
 
 
@@ -50,11 +60,19 @@ class CreateLocations extends Command
         }
     }
 
+    /**
+     * Create new locations
+     * 
+     * @param String $location_type_field
+     * @param String $attr
+     */
     private function AddRemoveLocations($location_type_field, $attr)
     {       
-        // Read from config
-        $excludedCounties = [];
-        $excludedCities = [];
+        // Read from config : settings
+        $excluded_locations = collect([
+            'counties' => [],
+            'cities' => []
+        ]);
 
         // Locations
         $locations = Listing::select($location_type_field)
@@ -63,21 +81,32 @@ class CreateLocations extends Command
 
         // Existing Locations
         $existing_locations = Listing::select($location_type_field)
-        ->leftJoin($attr['table_name'], $attr['table_name'].".".$attr['field_to_join'], '=', "real_estate_listings.{$location_type_field}")
+        ->leftJoin($attr['table_name'], $attr['table_name'].".name", '=', "real_estate_listings.{$location_type_field}")
         ->whereNotIn("real_estate_listings.{$location_type_field}", $locations)
         ->distinct()
         ->pluck($location_type_field)->toArray();
 
-        // New Locations to Add
+        
         $new_locations = collect(array_udiff($locations, $existing_locations, 'strcasecmp'));
 
-        
-        // foreach($new_locations as $location){
-        //     $location = Listing::where($attr, $location)->first();
-        //     $location->$attr()->save([
-        //         'name' =>  $location,
-        //         'slug' => str_slug($add_name, '-')
-        //         ]);          
-        // }
+        // Filter excluded locations and get new array of locations
+        $new_locations_arr = $new_locations->map(function ($location) use ($excluded_locations){
+            $isExcluded = false;
+            foreach($excluded_locations as $ex_locations){
+                $isExcluded = \Arr::has( $ex_locations, $location);
+            }            
+            return !$isExcluded? [
+                    'name' =>  $location,
+                    'slug' => str_slug($location, '-')
+                ]:false;
+        })->reject(function ($value) {
+            return $value === false;
+        });
+
+        // Create New Locations
+        if($new_locations_arr->count() > 0){
+            $locations_added = \DB::table($attr['table_name'])->insert($new_locations_arr->toArray());
+            $this->info('Created Locations:' . ((bool) $locations_added));
+        }            
     }
 }
