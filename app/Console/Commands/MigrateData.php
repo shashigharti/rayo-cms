6e32cb76bc84b4ef56ea3d426ebd76784489208d
Kundan Karna <mysoulsource.np@gmail.com>
2019-12-05 15:46:56 +0545

    migration data

--- app/Console/Commands/MigrateData.php
+++ app/Console/Commands/MigrateData.php
@@ -29,40 +29,92 @@ class MigrateData extends Command
      * @return mixed
      */
     public function handle()
-    {        
+    {
 
-        $listings = \DB::table('listings')
-        ->where('id','<', 7275)
-        ->get()->toArray();
-        foreach($listings as $key => $listing){   
-            $listing = json_decode(json_encode($listing), true);
-            $listing_fields =  array_keys($listing);      
-            Listing::create($listing);
-            foreach ($listing_fields as $field){
-                ListingProperty::create([
-                    'listing_id' => $listing['id'],
-                     'type' => $field, 
-                     'value' => $listing[$field],
-                     'status' => 1  //1 enabled, 0 disabled
-                     ]);           
-            }
-        }       
-        $listing_details = \DB::table('listing_details')
-        ->where('listing_id','<', 7275)
-        ->get()->toArray();
+//        $listings = \DB::table('listings')
+//        ->where('id','<', 7275)
+//        ->get()->toArray();
+//        foreach($listings as $key => $listing){
+//            $listing = json_decode(json_encode($listing), true);
+//            $listing_fields =  array_keys($listing);
+//            Listing::create($listing);
+//            foreach ($listing_fields as $field){
+//                ListingProperty::create([
+//                    'listing_id' => $listing['id'],
+//                     'type' => $field,
+//                     'value' => $listing[$field],
+//                     'status' => 1  //1 enabled, 0 disabled
+//                     ]);
+//            }
+//        }
+//        $listing_details = \DB::table('listing_details')
+//        ->where('listing_id','<', 7275)
+//        ->get()->toArray();
+//
+//        foreach($listing_details as $key => $listing_detail){
+//            $listing_detail = json_decode(json_encode($listing_detail), true);
+//            $listing_details_fields = array_keys($listing_detail);
+//            ListingDetail::create($listing_detail);
+//            foreach ($listing_details_fields as $field){
+//                ListingProperty::updateOrCreate([
+//                    'listing_id' => $listing_detail['listing_id'],
+//                    'type' => $field,
+//                    'value' => $listing_detail[$field],
+//                    'status' => 1  //1 enabled, 0 disabled
+//                    ]);
+//            }
+//        }
 
-        foreach($listing_details as $key => $listing_detail){            
-            $listing_detail = json_decode(json_encode($listing_detail), true);
-            $listing_details_fields = array_keys($listing_detail);
-            ListingDetail::create($listing_detail);
-            foreach ($listing_details_fields as $field){
-                ListingProperty::updateOrCreate([
-                    'listing_id' => $listing_detail['listing_id'],
-                    'type' => $field, 
-                    'value' => $listing_detail[$field],
-                    'status' => 1  //1 enabled, 0 disabled
-                    ]);           
+
+        //in live we already have table as real_estate_listings no need to create new
+
+        $removable = ['id','class','system_price','city_id','zip_id',
+            'subdivision_id','county_id','picture_count','created_at','updated_at',
+            'listing_id'];
+        Listing::chunk(5000,function ($listings) use ($removable){
+            foreach ($listings as $listing)
+            {
+                $listing = json_decode($listing,true);
+                $listing_fields = array_keys($listing);
+                foreach ($listing_fields as $field)
+                {
+                    if(
+                        $listing[$field] != null
+                        && !in_array($listing[$field],['','None','none','undefined'])
+                        &&  !in_array($field,$removable)){
+
+                        $property = ListingProperty::create([
+                            'listing_id' => $listing['id'],
+                            'type' => $field,
+                            'value' => $listing[$field],
+                            'status' => 1  //1 enabled, 0 disabled
+                        ]);
+                   }
+                }
             }
-        }       
+        });
+        \DB::table('real_estate_listing_details')
+            ->orderBy('id')
+            ->chunk(5000,function ($listings) use ($removable){
+            foreach ($listings as $listing)
+            {
+                $listing = (array) $listing;
+                $listing_fields = array_keys($listing);
+                foreach ($listing_fields as $field)
+                {
+                    if(
+                        $listing[$field] != null
+                        && !in_array($listing[$field],['','None','none','undefined'])
+                        &&  !in_array($field,$removable)){
+                        $property = ListingProperty::create([
+                            'listing_id' => $listing['listing_id'],
+                            'type' => $field,
+                            'value' => $listing[$field],
+                            'status' => 1  //1 enabled, 0 disabled
+                        ]);
+                    }
+                }
+            }
+        });
     }
 }
