d656649ab485ad16f3d934549385ef20de1decb0
Kundan Karna <mysoulsource.np@gmail.com>
2019-12-05 16:41:01 +0545

    market design fixes

--- packages/robust/real-estate/resources/views/website/market-report/insight.blade.php
+++ packages/robust/real-estate/resources/views/website/market-report/insight.blade.php
@@ -5,51 +5,54 @@
 @endsection
 @section('body_section')
     <section class="market market-insight main-content" data-page='{{$page_type}}'>
-        
+
         @if(isset($data['records']))
             <div class="container-fluid">
                 @include(Site::templateResolver('real-estate::website.market-report.partials.info'))
                 @include(Site::templateResolver('real-estate::website.market-report.partials.tool-box'))
                 @include(Site::templateResolver('real-estate::website.market-report.partials.locations'),
-                ['records' => $data['records']??[]]) 
+                ['records' => $data['records']??[]])
             </div>
         @endif
+        <div class="container-fluid">
+            <div class="market-insight__stat">
+                <h4> Monthly Summary Report </h4>
+                <div class="market-insight__stat-container">
+                    <table class="table striped">
+                        <thead>
+                            <tr>
+                                <th>SN</th>
+                                <th class="text-center">#Props Active</th>
+                                <th class="text-center">#Props Sold</th>
+                                <th class="text-center">Average Price</th>
+                                <th class="text-center">Avg. Days List-Sell</th>
+                                <th class="text-center">%Sale-to-List Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="text-center">
+                                <th scope="text-left">5</th>
+                                <td>10</td>
+                                <td>11</td>
+                                <td>12</td>
+                                <td>13</td>
+                                <td>14</td>
+                            </tr>
+                            @foreach ($data['insights'] as $insight)
 
-        <div class="row market-insight__stat">
-            <h1> Monthly Summary Report </h1>
-            <table class="table striped">
-                <thead>
-                    <tr>
-                        <th>SN</th>
-                        <th class="text-center">#Props Active</th>
-                        <th class="text-center">#Props Sold</th>
-                        <th class="text-center">Average Price</th>
-                        <th class="text-center">Avg. Days List-Sell</th>
-                        <th class="text-center">%Sale-to-List Price</th>
-                    </tr>
-                </thead>            
-                <tbody>
-                    <tr class="text-center">
-                        <th scope="text-left">5</th>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                        <td>14</td>
-                    </tr>
-                    @foreach ($data['insights'] as $insight)
-                    
-                        <tr class="text-center">
-                            <th scope="text-left">{{ $insight->active_count}}</th>
-                            <td>{{ $insight->system_price}}</td>
-                            <td>{{ $insight->system_price}}</td>
-                            <td>{{ $insight->system_price}}</td>
-                            <td>{{ $insight->system_price}}</td>
-                            <td>{{ $insight->system_price}}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                <tr class="text-center">
+                                    <th scope="text-left">{{ $insight->active_count}}</th>
+                                    <td>{{ $insight->system_price}}</td>
+                                    <td>{{ $insight->system_price}}</td>
+                                    <td>{{ $insight->system_price}}</td>
+                                    <td>{{ $insight->system_price}}</td>
+                                    <td>{{ $insight->system_price}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
