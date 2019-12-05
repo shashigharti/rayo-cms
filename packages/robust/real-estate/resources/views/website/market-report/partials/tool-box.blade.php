d64076d7003efbf08ef1e27bfaca616a8a57681f
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-12-05 13:19:21 +0545

    Fixed market report page
    1.
    Fixed single block click for cities and subdivisions

--- packages/robust/real-estate/resources/views/website/market-report/partials/tool-box.blade.php
+++ packages/robust/real-estate/resources/views/website/market-report/partials/tool-box.blade.php
@@ -2,12 +2,12 @@
     <div id="market--left__sort" class="market--left__sort col s6">
         <p><b>Sort By:</b></p>
         <div class="market--left__sort--btns">
-            <a href="#" data-type="Active" class="active" data-status="active">#Active Listings</a>
-            <a href="#" data-type="Average" data-status="inactive">Average Price Sold</a>
-            <a href="#" data-type="Median" data-status="inactive">Median Price Sold</a>
-            <a href="#" data-type="Sold" data-status="inactive">#Sold Listings</a>
-            <a href="#" data-type="Title" data-status="inactive">Alphabetically</a>
-            <a href="#" data-type="Priority" data-locations="" data-status="inactive">Priority Location</a>
+            <a href="javascript:void(0)" data-type="Active" class="active" data-status="active">#Active Listings</a>
+            <a href="javascript:void(0)" data-type="Average" data-status="inactive">Average Price Sold</a>
+            <a href="javascript:void(0)" data-type="Median" data-status="inactive">Median Price Sold</a>
+            <a href="javascript:void(0)" data-type="Sold" data-status="inactive">#Sold Listings</a>
+            <a href="javascript:void(0)" data-type="Title" data-status="inactive">Alphabetically</a>
+            <a href="javascript:void(0)" data-type="Priority" data-locations="" data-status="inactive">Priority Location</a>
         </div>
     </div>
     <div id="market--right__display" class="market--right__display col s6">
