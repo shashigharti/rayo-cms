a466e3407f956b89258c585d8b1da3e399b58932
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-12-10 21:41:52 +0545

    removed duplicate code

--- resources/assets/website/js/app.js
+++ resources/assets/website/js/app.js
@@ -1,5 +1,4 @@
 //import("../../../../node_modules/owl.carousel/dist/owl.carousel.js")
-//import("../../../../node_modules/owl.carousel/dist/owl.carousel.js")
 //import("modules/jquery.range.js")
 //import("modules/materialize.js")
 //import("modules/main.js")
