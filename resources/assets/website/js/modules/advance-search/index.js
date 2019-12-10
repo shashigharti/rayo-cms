613f69e724087444b9e42156b05a95b660da313b
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-12-10 15:43:18 +0545

    removed unused code

--- resources/assets/website/js/modules/advance-search/index.js
+++
@@ -1,5 +0,0 @@
-import SearchDropdown from "./SearchDropdown";
-import AdvanceSearch from "./AdvanceSearch";
-import AdvanceSearchDialog from "./AdvanceSearchDialog"
-
-export { SearchDropdown, AdvanceSearch, AdvanceSearchDialog };
--- resources/assets/website/js/modules/test.js
+++
@@ -1,1 +0,0 @@
-import { AdvanceSearch } from './advance-search';
