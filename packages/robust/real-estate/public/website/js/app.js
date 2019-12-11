4035e6441551f9fc5ec05b528a8382305802bb14
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-12-11 20:19:32 +0545

    Rearranged select dropdown file
    1.
    Removed select dropdown from real-estate to core

--- packages/robust/real-estate/public/website/js/app.js
+++ packages/robust/real-estate/public/website/js/app.js
@@ -1,4 +1,3 @@
 //import("modules/market-report.js")
 //import("modules/market-survey.js")
 //import("modules/advance-search.js")
-//import("modules/select.js")
--- packages/robust/real-estate/public/website/js/modules/advance-search.js
+++ packages/robust/real-estate/public/website/js/modules/advance-search.js
@@ -12,10 +12,17 @@
                 $('#adv-search-dropdown').toggleClass('show');
             });
 
-            // $('#adv-search-dropdown select').each((index, elem) => {
-            //     //console.log($(elem).data('selected'));
-            //     console.log($(elem).data('selected'));
-            // });
+            $('#adv-search-dropdown select').each((index, elem) => {
+                let selectedElements = [];
+                //elem = $(elem);
+
+                // if (elem.data('selected')) {
+                //     selectedElements = elem.data('selected').split(",");
+                //     console.log(selectedElements);
+                // }
+                // // console.log($(elem).data('selected'));
+                // console.log($(elem).data('selected'));
+            });
         }
     });
 }(jQuery, FRW, window, document));
