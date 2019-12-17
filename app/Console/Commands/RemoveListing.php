68ea80189062ba6e1aae13c43b70be337455b99d
Kundan Karna <mysoulsource.np@gmail.com>
2019-12-17 16:06:22 +0545

    truncate datas

--- app/Console/Commands/RemoveListing.php
+++ app/Console/Commands/RemoveListing.php
@@ -4,10 +4,15 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use Robust\RealEstate\Models\City;
+use Robust\RealEstate\Models\County;
+use Robust\RealEstate\Models\HighSchool;
 use Robust\RealEstate\Models\Listing;
 use Robust\RealEstate\Models\ListingImages;
 use Robust\RealEstate\Models\ListingProperty;
 use Robust\RealEstate\Models\Location;
+use Robust\RealEstate\Models\Subdivision;
+use Robust\RealEstate\Models\Zip;
 
 class RemoveListing extends Command
 {
@@ -56,29 +61,52 @@ class RemoveListing extends Command
      */
     public function handle()
     {
-        $total = Listing::count();
-        $counter = 0;
-        $removed =0;
-        Listing::chunk(1000,function ($listings) use ($counter,$removed,$total){
-           foreach ($listings as $listing){
-               $counter+=1;
-
-               $city = Location::where('id',$listing->city_id)
-                        ->where('locationable_type',self::RELATION_MAP['cities']['class'])->first();
-               if($city && in_array($city->name,$this->cities) && $listing->system_price > 10000){
-                   $this->info('Matched : ' .$city->name . ' || Price : ' .$listing->system_price);
-               }else{
-                   if($city){
-                       $this->info('Unmatched : ' . $city->name);
-                   }
-                   $removed+=1;
-                   $listing->delete();
-                   ListingProperty::where('listing_id',$listing->id)->delete();
-                   ListingImages::where('listing_id',$listing->id)->delete();
-               }
-               $this->info('Total : ' . $total . ' || Counter : ' .$counter . ' || Removed : ' .$removed);
-           }
-        });
-
+        $this->info('Truncating Listings');
+        Listing::truncate();
+        $this->info('Done Listings');
+        $this->info('Truncating Cities');
+        City::truncate();
+        $this->info('Done Cities');
+        $this->info('Truncating Properties');
+        ListingProperty::truncate();
+        $this->info('Done Properties');
+        $this->info('Truncating Images');
+        ListingImages::truncate();
+        $this->info('Done Images');
+        $this->info('Truncating Location');
+        Location::truncate();
+        $this->info('Done Location');
+        $this->info('Truncating Subdivision');
+        Subdivision::truncate();
+        $this->info('Done Subdivision');
+        $this->info('Truncating Zip');
+        Zip::truncate();
+        $this->info('Done Zip');
+        $this->info('Truncating County');
+        County::truncate();
+        $this->info('Done');
+//        $total = Listing::count();
+//        $counter = 0;
+//        $removed =0;
+//        Listing::chunk(1000,function ($listings) use ($counter,$removed,$total){
+//           foreach ($listings as $listing){
+//               $counter+=1;
+//
+//               $city = Location::where('id',$listing->city_id)
+//                        ->where('locationable_type',self::RELATION_MAP['cities']['class'])->first();
+//               if($city && in_array($city->name,$this->cities) && $listing->system_price > 10000){
+//                   $this->info('Matched : ' .$city->name . ' || Price : ' .$listing->system_price);
+//               }else{
+//                   if($city){
+//                       $this->info('Unmatched : ' . $city->name);
+//                   }
+//                   $removed+=1;
+//                   $listing->delete();
+//                   ListingProperty::where('listing_id',$listing->id)->delete();
+//                   ListingImages::where('listing_id',$listing->id)->delete();
+//               }
+//               $this->info('Total : ' . $total . ' || Counter : ' .$counter . ' || Removed : ' .$removed);
+//           }
+//        });
     }
 }
