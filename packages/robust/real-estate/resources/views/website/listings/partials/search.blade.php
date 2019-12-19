18ba3714b81c75f76662050cfe1d3f54cef209d9
Bhumika <ganeshbhumika876@gmail.com>
2019-12-19 13:32:55 +0545

    search-blade-fix

--- packages/robust/real-estate/resources/views/website/listings/partials/search.blade.php
+++ packages/robust/real-estate/resources/views/website/listings/partials/search.blade.php
@@ -6,7 +6,6 @@
                 <p>FEATURES</p>
                 <a href="#" class="theme-btn advance-search">Advanced search</a>
             </div>
-
             <div class="col s8">
                 <div class="row">
                     <div class="col s4 range-bar">
@@ -29,36 +28,35 @@
             </div>
         </div>
     </div>
-</div>
-<div class="listing--btn--block col s12">
-    <div class="row">
-        <div class="col m7 s12">
-            <label>Sort By :</label>
-            <select name="sort">
-                @foreach($sort_settings as $sort)
-                <option value="{{$sort['value']}}">{{$sort['display']}}</option>
-                @endforeach
-            </select>
-            <a href="{{route('website.realestate.sold-homes')}}" class="btn cyan">SOLD</a>
-            <a href="{{route('website.realestate.homes-for-sale')}}" class="btn green">ACTIVE</a>
-        </div>
-        <div class="col m5 s12 right-align">
-            <div class="total--records">
-                <span>{{$results->perPage()}}</span> of <span>{{$results->total()}}</span> Total Properties
+    <div class="listing--btn--block col s12">
+        <div class="row">
+            <div class="col m7 s12">
+                <label>Sort By :</label>
+                <select name="sort">
+                    @foreach($sort_settings as $sort)
+                    <option value="{{$sort['value']}}">{{$sort['display']}}</option>
+                    @endforeach
+                </select>
+                <a href="{{route('website.realestate.sold-homes')}}" class="btn cyan">SOLD</a>
+                <a href="{{route('website.realestate.homes-for-sale')}}" class="btn green">ACTIVE</a>
             </div>
-            <div class="listing--pagination">
-                <span><a href="{{$results->previousPageUrl()}}"><<</a></span>
-                <span class="active">{{$results->currentPage()}}</span>
-                <span><a href="{{$results->nextPageUrl()}}">>></a></span>
+            <div class="col m5 s12 right-align">
+                <div class="total--records">
+                    <span>{{$results->perPage()}}</span> of <span>{{$results->total()}}</span> Total Properties
+                </div>
+                <div class="listing--pagination">
+                    <span><a href="{{$results->previousPageUrl()}}"><<</a></span>
+                    <span class="active">{{$results->currentPage()}}</span>
+                    <span><a href="{{$results->nextPageUrl()}}">>></a></span>
+                </div>
             </div>
-        </div>
-        <div class="col s12 listing--tags">
-            <span>Active</span>
-            <span>Price : low to high</span>
-            <a href="#" class=" btn btn-sm theme-btn">CLEAR ALL</a>
+            <div class="col s12 listing--tags">
+                <span>Active</span>
+                <span>Price : low to high</span>
+                <a href="#" class=" btn btn-sm theme-btn">CLEAR ALL</a>
+            </div>
         </div>
     </div>
-</div>
 </form>
 
 <div class="listing--advance_search">
