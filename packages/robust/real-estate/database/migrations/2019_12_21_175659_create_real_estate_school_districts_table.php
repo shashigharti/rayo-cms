3d78aed8f44d3bb8adbd0b613f2eb148f746d39d
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2019-12-21 23:44:52 +0545

    Added migration for school districts

---
+++ packages/robust/real-estate/database/migrations/2019_12_21_175659_create_real_estate_school_districts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRealEstateSchoolDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_estate_school_districts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_estate_school_districts');
+    }
+}
