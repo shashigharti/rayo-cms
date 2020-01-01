31f1c5a6781e05b899d89110564651bed73f3c0d
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-01 20:51:00 +0545

    Fixed routes:
    1. login,
    2. logout
    3. register
    4. passwords
    5. verification

--- packages/robust/admin/routes/website/auth.php
+++ packages/robust/admin/routes/website/auth.php
@@ -1,19 +1,18 @@
 <?php
 Route::group(['prefix' => config('core.frw.auth'), 'as' => 'website.auth.','group' => 'Auth'], function () {
     Route::get('login', [
-        'name' => 'Login',
         'as' => 'login',
         'uses' => 'Robust\Admin\Controllers\Website\Auth\LoginController@login'
     ]);
     Route::post('login', [
-        'as' => 'post_login',
+        'as' => 'login.post',
         'uses' => 'Robust\Admin\Controllers\Website\Auth\LoginController@postLogin'
     ]);
-    // Route::get('logout', [
-    //     'name' => 'Logout',
-    //     'as' => 'logout',
-    //     'uses' => 'Robust\Admin\Controllers\Website\Auth\LoginController@logout'
-    // ]);
+    Route::get('logout', [
+        'name' => 'Logout',
+        'as' => 'logout',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\LoginController@logout'
+    ]);
     
     Route::get('register', [
         'as' => 'register',
@@ -21,7 +20,49 @@ Route::group(['prefix' => config('core.frw.auth'), 'as' => 'website.auth.','grou
     ]);
     
     Route::post('register', [
-        'as' => 'post_register',
+        'as' => 'register.post',
         'uses' => 'Robust\Admin\Controllers\Website\Auth\RegisterController@postRegister'
     ]);
+
+
+
+    Route::get('password/reset', [
+        'as' => 'password.request',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\ForgotPasswordController@showLinkRequestForm'
+    ]);
+    
+    Route::post('password/email', [
+        'as' => 'password.email',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\ForgotPasswordController@sendResetLinkEmail'
+    ]);
+
+    Route::get('password/reset/{token}', [
+        'as' => 'password.reset',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\ForgotPasswordController@showResetForm'
+    ]);
+    
+    Route::post('password/reset', [
+        'as' => 'password.update',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\ResetPasswordController@reset'
+    ]);
+
 });
+
+Route::group(['prefix' => '', 'as' => 'website.auth.verification.','group' => 'Verification'], function () {
+    Route::get('/email/verify', [
+        'name' => 'verification notice',
+        'as' => 'notice',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\VerificationController@show'
+    ]);
+    Route::get('/email/verify/{id}', [
+        'name' => 'verify',
+        'as' => 'verify',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\VerificationController@verify'
+    ]);
+    Route::get('/email/resend', [
+        'name' => 'resend',
+        'as' => 'resend',
+        'uses' => 'Robust\Admin\Controllers\Website\Auth\VerificationController@resend'
+    ]);
+});
+
