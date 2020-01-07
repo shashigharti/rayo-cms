4367057a15cba46172fa0e6e6ee5b32ddec587a4
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-07 15:05:23 +0545

    fixed google tag settings

--- packages/robust/core/resources/views/admin/settings/ga-analytics.blade.php
+++ packages/robust/core/resources/views/admin/settings/ga-analytics.blade.php
@@ -15,16 +15,25 @@
                 {{ Form::label('code', 'Head Script', ['class' => 'control-label' ]) }}
             </div>
         </div>
-
         <div class="form-group form-material row">
             <div class="col s12 input-field">
-                {{ Form::label('script-after-body-opening', 'Script after body tag', ['class' => 'control-label' ]) }}
+                {{ Form::label('script-after-body-opening', 'Script after body opening tag', ['class' => 'control-label' ]) }}
                 {{ Form::textarea('script-after-body-opening', isset($settings['script-after-body-opening'])?$settings['script-after-body-opening']:'', [
                         'class' => 'form-control',
                         'rows' => 3
                     ]) 
                 }}
                 {{ Form::label('body-script', 'Body Script', ['class' => 'control-label' ]) }}
+            </div>
+        </div>
+         <div class="form-group form-material row">
+            <div class="col s12 input-field">
+                {{ Form::label('script-before-body-closing', 'Script after body closing tag', ['class' => 'control-label' ]) }}
+                {{ Form::textarea('script-before-body-closing', isset($settings['script-before-body-closing'])?$settings['script-before-body-closing']:'', [
+                        'class' => 'form-control',
+                        'rows' => 3
+                    ]) 
+                }}
             </div>
         </div>
         <div class="form-group form-material">
