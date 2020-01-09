2e72b80d680345082c04bc7440998d4a9c23a7d9
Bhumika <ganeshbhumika876@gmail.com>
2020-01-09 14:04:51 +0545

    data-mapping-design-fix

--- packages/robust/real-estate/resources/views/admin/settings/data-mapping.blade.php
+++ packages/robust/real-estate/resources/views/admin/settings/data-mapping.blade.php
@@ -4,14 +4,14 @@
         <fieldset>
             <legend>Data Server (RETS) </legend>
             <div class="form-group form-material row">
-                <div class="col s6">
+                <div class="col s6 input-field">
                     {{ Form::label('user_id', 'User ID', ['class' => 'control-label' ]) }}
                     {{ Form::text('user_id', isset($settings['user_id'])?$settings['user_id']:'', [
                             'class' => 'form-control'
                         ]) 
                     }}
                 </div>
-                <div class="col s6">
+                <div class="col s6 input-field">
                     {{ Form::label('password', 'Password', ['class' => 'control-label' ]) }}
                     {{ Form::text('password', isset($settings['password'])?$settings['password']:'', [
                             'class' => 'form-control'
@@ -20,8 +20,10 @@
                 </div>
             </div>
         </fieldset>        
-        <div class="form-group form-material">
-            {{ Form::submit($ui->getSubmitText(), ['class' => 'btn btn-primary theme-btn']) }}
+        <div class="form-group form-material row mt-3">
+            <div class="col s12">
+                {{ Form::submit($ui->getSubmitText(), ['class' => 'btn btn-primary theme-btn']) }}
+            </div>
         </div>
     {{Form::close()}}
 </div>
