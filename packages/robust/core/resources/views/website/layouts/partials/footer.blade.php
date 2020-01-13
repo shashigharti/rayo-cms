7f3b2b8457cfdbc72098cfd1f6c45d28e77c2c15
Manisha <maansi.chaudhary3@gmail.com>
2020-01-13 22:16:32 +0545

    fixed helpers

--- packages/robust/core/resources/views/website/layouts/partials/footer.blade.php
+++ packages/robust/core/resources/views/website/layouts/partials/footer.blade.php
@@ -1,5 +1,6 @@
-@inject('page_helper, 'Robust\RealEstate\Helpers\PageHelper')
+@inject('page_helper', 'Robust\RealEstate\Helpers\PageHelper')
 @set('links',$page_helper->getLinksByType('useful-links'))
+@set('services',$page_helper->getLinksByType('info-services'))
 <div class="container-fluid">
     <div class="row">
         <div class="col m4 s12">
@@ -9,27 +10,22 @@
         <div class="col m4 s12">
             <h3>useful links</h3>
             @if(isset($links))
-                @foreach($links as $links)
-                    <a href="{{route('website.realestate.pages.details',['slug'=>$links->slug])}}">
-                        <p>{{ $links->title }}</p>
+                @foreach($links as $link)
+                    <a href="{{route('website.realestate.pages.details',['slug'=>$link->slug])}}">
+                        <p>{{ $link->title }}</p>
                     </a>
                 @endforeach
             @endif
         </div>
         <div class="col m4 s12">
             <h3>info & services</h3>
-            <a href="#">
-                <p>HOMES FOR SALE</p>
-            </a>
-            <a href="#">
-                <p>SOLD HOMES</p>
-            </a>
-            <a href="#">
-                <p>ACTIVE PROPERTIES</p>
-            </a>
-            <a href="#">
-                <p>SEARCH LISTINGS</p>
-            </a>
+            @if(isset($services))
+                @foreach($services as $service)
+                    <a href="{{route('website.realestate.pages.details',['slug'=>$service->slug])}}">
+                        <p>{{ $service->title }}</p>
+                    </a>
+                @endforeach
+            @endif
         </div>
     </div>
 </div>
