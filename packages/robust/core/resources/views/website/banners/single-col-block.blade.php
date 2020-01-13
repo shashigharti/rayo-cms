b84c24cd115e645c27ef7ed3c70c3a1880682d50
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-13 12:49:01 +0545

    code refactored

--- packages/robust/core/resources/views/website/banners/single-col-block.blade.php
+++ packages/robust/core/resources/views/website/banners/single-col-block.blade.php
@@ -1,43 +1,39 @@
-@set('singleColBanners', $banner_helper->getBannersByType(['single-col-block']))
-<section class="search-lists">
-    <div class="container-fluid">
-        <div class="row">
-            @foreach($singleColBanners as $singleColBanner)
-                @set('properties', json_decode($singleColBanner->properties,true))
-                @set('location',$location_helper->getLocation($properties['locations']))
-                @set('image',$listing_helper->getImageByLocation($location,$properties['image'] ?? ''))
-                @if($properties && $location)
-                    <div class="col m4 s12">
-                        <div class="single-block">
-                            <img src="{{$image}}" alt="{{$location->name}}">
-                            <div class="figcaption center-align">
-                                <h2>{{$location->name}}</h2>
-                                <div class="available-prices">
-                                    @if(isset($properties['prices']) && is_array($properties['prices']))
-                                        @foreach($properties['prices'] as  $price => $count)
-                                            @set('property_count',$properties->property_counts->$price ?? 0)
-                                            <a href="{{route('website.realestate.homes-for-sale',
+@set('properties', json_decode($singleColBanner->properties,true))
+@set('location',$location_helper->getLocation($properties['locations']))
+@set('image',$listing_helper->getImageByLocation($location,$properties['image'] ?? ''))
+@if($properties && $location)
+    <div class="col m4 s12">
+        <div class="single-block">
+            <img src="{{$image}}" alt="{{$location->name}}">
+            <div class="figcaption center-align">
+                <h2>{{$location->name}}</h2>
+                <div class="available-prices">
+                    @if(isset($properties['prices']) && is_array($properties['prices']))
+                        @foreach($properties['prices'] as  $price => $count)
+                            @set('property_count',$properties->property_counts->$price ?? 0)
+                            <a href="{{route('website.realestate.homes-for-sale',
                                                 [
                                                     'location_type' => get_location_route_by_type($location->locationable_type),
                                                     'location' => $location->slug,
                                                     'price' => $price,
                                                 ]
                                                 )}}"> {{$price}} ({{$count}})</a>
-                                        @endforeach
-                                    @endif
-                                </div>
-                                @if(isset($properties['tabs_data']) && is_array($properties['tabs']))
-                                    <div class="subdivs--list__block">
-                                        @foreach($properties['tabs_data'] as $key => $tabs)
-                                            <div class="subdivs--list__btn">
-                                                <i class="material-icons">redo</i>
-                                                <span class="subdivs--list__text">See</span> {{$key}}
-                                                @set('tab_fields',[])
-                                                <div class="subdivs--list">
-                                                    <p><label>{{$key}}:</label></p>
-                                                    <ul>
-                                                       @foreach($tabs as $tab => $count)
-                                                            <li><a href="{{route('website.realestate.homes-for-sale',
+                        @endforeach
+                    @endif
+                </div>
+                @if(isset($properties['tabs_data']) && is_array($properties['tabs']))
+                    <div class="subdivs--list__block">
+                        @foreach($properties['tabs_data'] as $key => $tabs)
+                            <div class="subdivs--list__btn">
+                                <i class="material-icons">redo</i>
+                                <span class="subdivs--list__text">See</span> {{$key}}
+                                @set('tab_fields',[])
+                                <div class="subdivs--list">
+                                    <p><label>{{$key}}:</label></p>
+                                    <ul>
+                                        @foreach($tabs as $tab => $count)
+                                            <li>
+                                                <a href="{{route('website.realestate.homes-for-sale',
                                                                     [
                                                                         'location_type'=>'cities',
                                                                         'location' =>  $location->slug,
@@ -45,22 +41,18 @@
                                                                         'sub_area' => $key
                                                                      ]
                                                                      )}}">
-                                                                    {{$tab}} ({{$count}})
-                                                                </a>
-                                                            </li>
-                                                       @endforeach
-                                                    </ul>
-                                                </div>
-                                            </div>
+                                                    {{$tab}} ({{$count}})
+                                                </a>
+                                            </li>
                                         @endforeach
-                                    </div>
-                                @endif
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
+                        @endforeach
                     </div>
                 @endif
-            @endforeach
+            </div>
         </div>
     </div>
-</section>
+@endif
 
--- packages/robust/core/resources/views/website/layouts/partials/ad-banners.blade.php
+++ packages/robust/core/resources/views/website/layouts/partials/ad-banners.blade.php
@@ -1,8 +1,19 @@
-@set('adBanners', $banner_helper->getBannersNotInType(['banner-slider','single-col-block','slider','two-col-ad']))
+@set('singleColBanners', $banner_helper->getBannersByType(['single-col-block']))
+<section class="search-lists">
+    <div class="container-fluid">
+        <div class="row">
+            @foreach($singleColBanners as $singleColBanner)
+                @include(Site::templateResolver("core::website.banners.single-col-block", ['properties' => $singleColBanner->properties]))
+            @endforeach
+        </div>
+    </div>
+</section>
 
-@include(Site::templateResolver("core::website.banners.single-col-block"))
-@include(Site::templateResolver("core::website.banners.two-col-ad"))
-@include(Site::templateResolver("core::website.banners.slider"))
-@foreach($adBanners as $adBanner)
-    @include(Site::templateResolver("core::website.banners.{$adBanner->template}"))
-@endforeach
+
+@set('adBanners', $banner_helper->getBannersNotInType(['banner-slider','single-col-block','slider','two-col-ad']))
+{{--@include(Site::templateResolver("core::website.banners.single-col-block"))--}}
+{{--@include(Site::templateResolver("core::website.banners.two-col-ad"))--}}
+{{--@include(Site::templateResolver("core::website.banners.slider"))--}}
+{{--@foreach($adBanners as $adBanner)--}}
+    {{--@include(Site::templateResolver("core::website.banners.{$adBanner->template}"))--}}
+{{--@endforeach--}}
