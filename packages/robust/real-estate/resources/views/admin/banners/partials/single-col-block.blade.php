3a98ee2f2044d0ffba9d22a96f04d5cfe20d39f2
Kundan Karna <mysoulsource.np@gmail.com>
2020-01-13 18:20:56 +0545

    with type code added:

--- packages/robust/real-estate/resources/views/admin/banners/partials/single-col-block.blade.php
+++ packages/robust/real-estate/resources/views/admin/banners/partials/single-col-block.blade.php
@@ -1,3 +1,4 @@
+@inject('advancesearch_helper', '\Robust\RealEstate\Helpers\AdvanceSearchHelper')
 <div class="row">
     <div class="input-field col s12">
         {{ Form::label('header', 'Header', ['class' => 'required' ]) }}
@@ -16,6 +17,35 @@
                 'data-url' => route('api.locations'),
                 'data-selected' => implode(",", $properties->locations ?? []),
                 'class'=>'browser-default multi-select ad-search-field',
+                'multiple'
+            ])
+        }}
+    </div>
+</div>
+<div class="row">
+    <div class="input-field col s4">
+        {{ Form::label('properties[property_type][]', 'Property Type', ['class' => 'control-label' ]) }}
+        {{ Form::select("properties[property_type][]", [
+                'property_type' => 'Property Type',
+                'construction_status' => 'Construction Status'
+            ],
+            request()->query('attribute') ?? $properties->property_type ?? '',
+            [
+                'class'=>'browser-default multi-select dynamic-attr_type',
+                'data-url-to-reload' => url()->current(),
+                'multiple'
+            ])
+        }}
+    </div>
+    <div class="input-field col s4">
+        @set('attribute', request()->query('attribute') ?? $properties->property_type[0] ?? '')
+        {{ Form::label('properties[property_value]', 'Property Values', ['class' => 'control-label' ]) }}
+        {{ Form::select("properties[property_value][]", [],
+            '',
+            [
+                'class'=>'browser-default multi-select',
+                'data-url'=> route('api.realestate.attributes.property_name',  [$attribute]),
+                'data-selected' => implode(',',$properties->property_value ?? []),
                 'multiple'
             ])
         }}
