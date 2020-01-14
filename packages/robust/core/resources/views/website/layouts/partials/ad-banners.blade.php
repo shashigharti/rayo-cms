56e6be5d9feabe6a4ca8091c8060e8d3e0082a38
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-14 11:02:47 +0545

    single col block rename variable

--- packages/robust/core/resources/views/website/layouts/partials/ad-banners.blade.php
+++ packages/robust/core/resources/views/website/layouts/partials/ad-banners.blade.php
@@ -1,18 +1,18 @@
-@set('singleColBanners', $banner_helper->getBannersByType(['single-col-block']))
+@set('singleColBlocks', $banner_helper->getBannersByType(['single-col-block']))
 <section class="search-lists">
     <div class="container-fluid">
         <div class="row">
             @set('col',settings('real-estate','banner_per_row') ? 12/settings('real-estate','banner_per_row') : 4)
-            @foreach($singleColBanners as $singleColBanner)
-                @set('properties',json_decode($singleColBanner->properties,true))
+            @foreach($singleColBlocks as $singleColBlock)
+                @set('properties',json_decode($singleColBlock->properties,true))
                 @if(isset($properties['property_type']) && count($properties['property_type']) > 0)
                     @include(Site::templateResolver("core::website.banners.single-col-block-with-type", [
-                        'properties' => $singleColBanner->properties,
+                        'properties' => $singleColBlock->properties,
                         'col' => $col
                      ]))
                 @else
                     @include(Site::templateResolver("core::website.banners.single-col-block", [
-                        'properties' => $singleColBanner->properties,
+                        'properties' => $singleColBlock->properties,
                         'col' => $col
                     ]))
                 @endif
