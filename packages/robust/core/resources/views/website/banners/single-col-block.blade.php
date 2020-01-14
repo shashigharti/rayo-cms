0ba25cccadf3718e15a1b13a4dad8e1ce4ac73a8
Kundan Karna <mysoulsource.np@gmail.com>
2020-01-14 11:25:46 +0545

    fixed single col banner

--- packages/robust/core/resources/views/website/banners/single-col-block.blade.php
+++ packages/robust/core/resources/views/website/banners/single-col-block.blade.php
@@ -1,4 +1,3 @@
-@set('properties', json_decode($singleColBanner->properties,true))
 @set('location',$location_helper->getLocation($properties['locations']))
 @set('image',$listing_helper->getImageByLocation($location,$properties['image'] ?? ''))
 @if($properties && $location)
