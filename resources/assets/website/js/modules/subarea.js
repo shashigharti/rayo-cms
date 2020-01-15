93d95cf68037ccf3b033c93d5ed0db22f0643415
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-15 18:23:15 +0545

    updated tabs

--- resources/assets/website/js/modules/subarea.js
+++ resources/assets/website/js/modules/subarea.js
@@ -5,7 +5,7 @@
         init: function() {
             $('.subdivs--list__btn').on('click',function () {
                 $('.subdivs--list').not($(this).children('.subdivs--list')).removeClass('show');
-                $('.subdivs--list__text').text('See');
+                $('.subdivs--list__text').text('');
                 $(this).children('.subdivs--list').toggleClass('show');
                 $(this).children('.subdivs--list__text').text('Hide');
             });
