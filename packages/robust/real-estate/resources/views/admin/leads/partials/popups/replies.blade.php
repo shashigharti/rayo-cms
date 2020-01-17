8ceb78af84154d03293f03401c79336346fc04de
Manisha <maansi.chaudhary3@gmail.com>
2020-01-17 15:43:11 +0545

    modified the leads

--- packages/robust/real-estate/resources/views/admin/leads/partials/popups/replies.blade.php
+++ packages/robust/real-estate/resources/views/admin/leads/partials/popups/replies.blade.php
@@ -6,6 +6,42 @@
         </small>
     </a>
     <ul id='dd-replies' class='dropdown-content'>
-        Testing
+        <div class="info-dialog view-box">
+            <div class="box-title">
+                Buyer
+                <i class="fa fa-times pull-right clickable"></i>
+            </div>
+            <div class="box-content">
+                <div class="row viewed-lead">
+                    <a href="//scottingraham.com/real-estate/692712/6903-lagoon-panama-city-beach-32408" target="_blank">
+                        <div class="col s4">
+                            <img src="http://cdn.photos.sparkplatform.com/bc/20200111182524426985000000-o.jpg" alt="6903 Lagoon, Panama City Beach 32408" class="img-responsive">
+                        </div>
+                        <div class="col s8">
+                            <div class="vw-lead-name">
+                                6903 Lagoon, Panama City Beach 32408
+                            </div>
+                            <div class="vw-lead-price">
+                                Price:$250k
+                                <br>
+                                Baths Full: 2
+                                <br>
+                                Beds
+                            </div>
+                            <div class="vw-lead-address">
+                                Address: 6903 Lagoon
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="row vw-view-more">
+                    <a href="#" class="">
+                        <div class="col s12">
+                            View more
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </ul>
 </div>
