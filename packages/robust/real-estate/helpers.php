99284238f55cb9b4fe53df60de413c288a37d685
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-20 19:48:06 +0545

    fixed price range display

--- packages/robust/real-estate/helpers.php
+++ packages/robust/real-estate/helpers.php
@@ -62,11 +62,11 @@ if (!function_exists('price_range_format')) {
             $prices = explode('>',$prices[0]);
         }
         $count = count_chars($prices[0]);
-        if ($count > 6 && (isset($prices[1]) && is_numeric($prices[1]))) {
+        if ($count > 6 && is_numeric($prices[1])) {
             $prices[0] = number_format($prices[0]);
             $prices[1] = number_format($prices[1]);
             return "$" . "{$prices[0]}-" . "$" . "{$prices[1]}";
-        } else {
+        } elseif($prices[1] == '') {
             if(isset($prices[0]) && is_numeric($prices[0])){
                 return "Above " . price_format($prices[0]);
             }
