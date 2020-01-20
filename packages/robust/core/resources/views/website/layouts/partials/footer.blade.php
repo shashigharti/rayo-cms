1ed4f73659f8a5cc8a2212c90f44f48d03ee28e8
Manisha <maansi.chaudhary3@gmail.com>
2020-01-20 11:18:55 +0545

    added dynamic footer

--- packages/robust/core/resources/views/website/layouts/partials/footer.blade.php
+++ packages/robust/core/resources/views/website/layouts/partials/footer.blade.php
@@ -1,6 +1,5 @@
-@inject('page_helper', 'Robust\RealEstate\Helpers\PageHelper')
-@set('links',$page_helper->getLinksByType('useful-links'))
-@set('services',$page_helper->getLinksByType('info-services'))
+@set('menus', settings('real-estate', 'menus') != ""? settings('real-estate', 'menus') : [])
+@inject('menu_helper', 'Robust\RealEstate\Helpers\FrontendMenuHelper')
 <div class="container-fluid">
     <div class="row">
         <div class="col m4 s12">
@@ -8,21 +7,29 @@
             {!!  settings('app-setting', 'footer_content')  !!}
         </div>
         <div class="col m4 s12">
-            <h3>useful links</h3>
-            @if(isset($links))
-                @foreach($links as $link)
-                    <a href="{{route('website.realestate.pages.details',['slug'=>$link->slug])}}">
-                        <p>{{ $link->title }}</p>
+            <h3>Useful Links</h3>
+            @set('menus', $banner_helper->getBannersBySlug('useful-links'))
+            @if($menus)
+                @set('properties', json_decode($menus->properties))
+                @set('titles', $properties->titles)
+                @set('urls', $properties->urls)
+                @foreach($titles as $key => $title)
+                    <a href="{{ $urls[$key] }}">
+                        <p>{{ $title }}</p>
                     </a>
                 @endforeach
             @endif
         </div>
         <div class="col m4 s12">
-            <h3>info & services</h3>
-            @if(isset($services))
-                @foreach($services as $service)
-                    <a href="{{route('website.realestate.pages.details',['slug'=>$service->slug])}}">
-                        <p>{{ $service->title }}</p>
+            <h3>Info & Services</h3>
+            @set('menus', $banner_helper->getBannersBySlug('info-services'))
+            @if($menus)
+                @set('properties', json_decode($menus->properties))
+                @set('titles', $properties->titles)
+                @set('urls', $properties->urls)
+                @foreach($titles as $key => $title)
+                    <a href="{{ $urls[$key] }}">
+                        <p>{{ $title }}</p>
                     </a>
                 @endforeach
             @endif
