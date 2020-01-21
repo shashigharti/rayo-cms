df519f5e5b8708072c0c1f85e27b3f2fbdbe31e9
Manisha <maansi.chaudhary3@gmail.com>
2020-01-21 15:06:05 +0545

    add followup using ajax

--- packages/robust/real-estate/resources/views/admin/leads/partials/popups/followups.blade.php
+++ packages/robust/real-estate/resources/views/admin/leads/partials/popups/followups.blade.php
@@ -9,26 +9,26 @@
                 <i class="fa fa-times pull-right clickable"></i>
             </div>
             <div class="box-content">
-                {{ Form::open(['route'=>['admin.leads.follow-up.store',$lead->id],'method'=>'post']) }}
-                {{ Form::hidden('lead_id',$lead->id) }}
-                <div class="row viewed-lead">
-                    <div class="col s12">
-                        {{ Form::text('date',null,['placeholder'=>'Select Date']) }}
+                <form class="FollowUpFom" data-id="{{$lead->id}}">
+                    {{ Form::hidden('lead_id',$lead->id) }}
+                    <div class="row viewed-lead">
+                        <div class="col s12">
+                            {{ Form::text('date',null,['placeholder'=>'Select Date']) }}
+                        </div>
+                        <div class="col s12 mt-3">
+                            {{ Form::Select('type',['Select Followup Type','Follow Up','To Do','Call Reminder'],null,['class'=>'form-control']) }}
+                        </div>
+                        <div class="col s12 mt-3">
+                            {{ Form::Select('agent_id',['Select Agent','Test']) }}
+                        </div>
+                        <div class="col s12">
+                            {{ Form::textarea('note',null,['placeholder'=>'notes']) }}
+                        </div>
+                        <div class="col s12 right-align mt-5">
+                            {{ Form::submit($ui->getSubmitText(), ['class' => 'waves-light theme-btn btn btn-follow-up']) }}
+                        </div>
                     </div>
-                    <div class="col s12 mt-3">
-                        {{ Form::Select('type',['Select Followup Type','Follow Up','To Do','Call Reminder'],null,['class'=>'form-control']) }}
-                    </div>
-                    <div class="col s12 mt-3">
-                        {{ Form::Select('agent_id',['Select Agent','Test']) }}
-                    </div>
-                    <div class="col s12">
-                        {{ Form::textarea('note',null,['placeholder'=>'notes']) }}
-                    </div>
-                    <div class="col s12 right-align mt-5">
-                        {{ Form::submit($ui->getSubmitText(), ['class' => 'waves-light theme-btn btn']) }}
-                    </div>
-                </div>
-                {{ Form::close() }}
+                </form>
             </div>
         </div>
     </ul>
