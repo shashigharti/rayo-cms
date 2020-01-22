253a4f43d49facdc7ad851ad4fd30693bdd01eea
Manisha <maansi.chaudhary3@gmail.com>
2020-01-22 11:59:53 +0545

    fixed followup js

--- packages/robust/real-estate/public/js/modules/popup.js
+++ packages/robust/real-estate/public/js/modules/popup.js
@@ -14,26 +14,6 @@
                 e.preventDefault();
                 $(this).closest('.popup-content').addClass('hide');
             })
-
-            $('.FollowUpFom').on('submit',function (e) {
-                e.preventDefault();
-                let lead_id = $(this).data('id');
-                $.ajax({
-                    type: 'POST',
-                    enctype: 'multipart/form-data',
-                    url: 'leads/' + lead_id + '/follow-up',
-                    dataType: "json",
-                    data: $('.FollowUpFom').serialize(),
-                    success: function (data) {
-                        console.log(data);
-                    },
-                    error: function (e) {
-                        alert("Error");
-                    }
-
-                });
-                $(this).closest('.popup-content').addClass('hide');
-            });
         },
     };
 
