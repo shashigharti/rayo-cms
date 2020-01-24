ee16db22b8206c40f21d98699da3be5a21c25e7a
Kundan Karna <mysoulsource.np@gmail.com>
2020-01-24 12:03:07 +0545

    email template fixed

--- packages/robust/real-estate/resources/views/admin/email-templates/create.blade.php
+++ packages/robust/real-estate/resources/views/admin/email-templates/create.blade.php
@@ -11,14 +11,14 @@
                     {{ Form::text('name', null, [
                             'placeholder' => 'Name i.e. \'KISAN\'',
                             'required'  => 'required'
-                        ]) 
+                        ])
                     }}
                 </div>
                 <div class="input-field col s6">
                     {{ Form::label('type', 'Type', ['class' => 'required' ]) }}
                     {{ Form::text('type', null, [
                             'placeholder' => 'Type i.e. \'Lead Import\''
-                        ]) 
+                        ])
                     }}
                 </div>
             </div>
@@ -43,7 +43,8 @@
                         ],
                         $template,
                         [
-                            'class' => 'select-reload-on-change'
+                            'class' => 'select-reload-on-change',
+                            'data-url-to-reload' => url()->current()
                         ])
                     }}
                     {{ Form::label('template', 'Select template to load', ['class' => 'required' ]) }}
@@ -55,29 +56,29 @@
                     {{ Form::textarea('subject', null, [
                             'placeholder' => 'subject i.e. \'Your South Central Alaska Home Search\'',
                             'required'  => 'required'
-                        ]) 
+                        ])
                     }}
                 </div>
             </div>
-            <div class="row editor">                
+            <div class="row editor">
                 <div class="input-field col s12">
-                    {{ Form::label('body', 'body', ['class' => 'required' ]) }}                    
-                    {{ Form::textarea('body', null, [
+                    {{ Form::label('body', 'body', ['class' => 'required' ]) }}
+                    {{ Form::textarea('body', $template != '' ? view('real-estate::admin.email-templates.partials.' .$template) : '', [
                             'placeholder' => 'Email body',
                             'required'  => 'required',
                             'id' => 'editor__body',
                             'class' => 'editor'
-                        ]) 
+                        ])
                     }}
                 </div>
-                <div class="col s12 editor__variables">                 
+                <div class="col s12 editor__variables">
                     *|LEAD_FIRSTNAME|* | *|LOGO|* | *|WEBSITE|* | *|VERIFICATION_LINK|*
                 </div>
             </div>
-            
+
             <div class="row mt-3">
                 <div class="col s12">
-                   {{ Form::submit($ui->getSubmitText(), ['class' => 'waves-light theme-btn btn']) }}           
+                   {{ Form::submit($ui->getSubmitText(), ['class' => 'waves-light theme-btn btn']) }}
                 </div>
             </div>
         </div>
