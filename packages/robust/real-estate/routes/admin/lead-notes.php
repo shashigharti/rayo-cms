6e3d74bf61671ff78cce19fff67a05ef91699969
Manisha <maansi.chaudhary3@gmail.com>
2020-01-28 14:54:47 +0545

    notes add edit functionality

--- packages/robust/real-estate/routes/admin/lead-notes.php
+++ packages/robust/real-estate/routes/admin/lead-notes.php
@@ -3,5 +3,11 @@ Route::group(['prefix' => config('core.frw.admin'), 'as' => 'admin.', 'group' =>
     Route::resources([
 		'notes' => 'Robust\RealEstate\Controllers\Admin\LeadNotesController'
     ]);
+
+    Route::post('leads/notes/{id}/update', [
+        'name' =>'Leads Notes',
+        'as' => 'leads.notes.update',
+        'uses' => '\Robust\RealEstate\Controllers\Admin\LeadNotesController@updateNotes'
+    ]);
 });
 
