ee7d2568aae69ae646ebfdc0f8433aba9f547ab8
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-31 15:32:45 +0545

    replaced js

--- packages/robust/core/resources/views/website/user-profile/partials/favourites.blade.php
+++ packages/robust/core/resources/views/website/user-profile/partials/favourites.blade.php
@@ -15,23 +15,38 @@
             </div>
         </div>
     </div>
-    <div class="favourite--table col s12 mr-t-20">
+    <div class="favourite__table col s12 mr-t-20">
         <table class="striped">
             <thead>
-            <tr>
-                <th>Name</th>
-                <th>Property Type</th>
-                <th>Asking Price</th>
-            </tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Property Type</th>
+                    <th>Asking Price</th>
+                </tr>
             </thead>
 
             <tbody>
                 @if(isset($lead->favourites) && !empty($lead->favourites))
-                    @foreach($lead->favourites as $favourite)
-                        <tr>
-                            <td>{{$favourite->name}}</td>
-                            <td>{{$favourite->class}}</td>
-                            <td>{{$favourite->system_price}}</td>
+                    @foreach($lead->favourites as $key => $listing)
+                        <tr data-id="{{ $listing->id }}">
+                            <td>
+                                <label>
+                                    {{ Form::checkbox("listing", $listing->slug) }}
+                                    <span>{{ $listing->name }}</span>
+                                </label>
+                            </td>
+                            <td>{{ $listing->class }}</td>
+                            <td>{{ $listing->system_price }}</td>
+                        </tr>
+                        <tr data-id="{{ $listing->id }}">
+                            <td>
+                                <label>
+                                    {{ Form::checkbox("listing", $listing->slug) }}
+                                    <span>{{ $listing->name }}</span>
+                                </label>
+                            </td>
+                            <td>{{ $listing->class }}</td>
+                            <td>{{ $listing->system_price }}</td>
                         </tr>
                     @endforeach
                 @endif
--- packages/robust/real-estate/public/website/js/app.js
+++ packages/robust/real-estate/public/website/js/app.js
@@ -1,3 +1,21 @@
+
+//import("../../../../node_modules/owl.carousel/dist/owl.carousel.js")
+//import("../../../../node_modules/leaflet/dist/leaflet.js");
+//import("../../../../node_modules/leaflet.markercluster/dist/leaflet.markercluster-src.js");
+//import("../../../../node_modules/leaflet.gridlayer.googlemutant/Leaflet.GoogleMutant.js");
+//import("modules/jquery.range.js")
+//import("modules/materialize.min.js")
+//import("modules/map.js")
+//import("modules/main.js")
+//import("modules/auth.js")
+//import("modules/dropdown.js")
+//import("modules/features.js")
+//import("modules/subarea.js")
+//import("modules/note.js")
+//import("modules/email.js")
+
+
+
 //import("modules/lmap.js")
 //import("modules/search.js")
 //import("modules/market-report.js")
@@ -5,3 +23,4 @@
 //import("modules/market-map-view.js")
 //import("modules/advance-search.js")
 //import("modules/tab-menu.js")
+//import("modules/user-profile.js")
