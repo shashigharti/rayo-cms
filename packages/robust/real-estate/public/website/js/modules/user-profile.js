1418d5c8b34518e35bf5db0c196f349a7285a8f8
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-01-31 16:05:14 +0545

    user profile

--- packages/robust/real-estate/public/website/js/modules/user-profile.js
+++ packages/robust/real-estate/public/website/js/modules/user-profile.js
@@ -2,6 +2,9 @@
 (function ($, FRW, window, document, undefined) {
     "use strict"
     $(function () {
+        $('.favourite__table').on('click', function(){
+            console.log('teseting');
+        });
 
     });
 }(jQuery, FRW, window, document));
