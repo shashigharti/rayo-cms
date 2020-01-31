c2bb7a056c572b97db66bf88d8c3686eceb9e5da
Manisha <maansi.chaudhary3@gmail.com>
2020-01-31 09:20:25 +0545

    fixed delete issues

--- packages/robust/core/public/js/modules/modal.js
+++ packages/robust/core/public/js/modules/modal.js
@@ -2,34 +2,28 @@
 (function ($, FRW, window, document, undefined) {
     'use strict';
     FRW.DeleteForm = {
-        confirmDelete: function(modalObj,buttonObj){
-            buttonObj.on('click', function (e) {
-                modalObj.find('.modal-body p').text(message);
-                let message = buttonObj.attr('data-message');
-                modalObj.find('.modal-body p').text(message);
-                let title = buttonObj.attr('data-title');
-                modalObj.find('.modal-title').text(title);
+        confirmDelete: function(modalObj){
+            const modal = $(modalObj);
+            M.Modal.init(modalObj,{onOpenStart: function (m,trigger) {
+                const button = $(trigger);
+                const modal = $(m);
+                let message = button.data('message');
+                modal.find('.modal-body p').text(message);
+                let title = button.attr('data-title');
+                modal.find('.modal-title').text(title);
                 // Pass form reference to modal for submission on yes/ok
-                let form = buttonObj.closest('form');
-                modalObj.find('.modal-footer #confirm').data('form', form);
-            });
-            modalObj.find('.modal-footer #confirm').on('click', function () {
+                let form = button.closest('form');
+                modal.find('.modal-footer #confirm').data('form', form);
+            }});
+            modal.find('.modal-footer #confirm').on('click', function () {
                 $(this).data('form').submit();
             });
-        }
-    };
-    FRW.Modal = {
-        init: function () {
-            $('.modal').modal();
-        }
-    };
+        },
+    }
     $(document).ready(function ($) {
         if ($('#confirmDelete').length > 0){
-            let modalObj = $('#confirmDelete');
-            let buttonObj = $('button[href="#confirmDelete"]');
-            FRW.DeleteForm.confirmDelete(modalObj,buttonObj);
+            const modal = document.getElementById('confirmDelete');
+            FRW.DeleteForm.confirmDelete(modal);
         }
-        FRW.Modal.init();
-        $('.modal').modal();
     });
 }(jQuery, FRW, window, document));
