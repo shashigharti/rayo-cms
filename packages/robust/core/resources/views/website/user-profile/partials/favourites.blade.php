7e790a6ddbdcb4f75ba7691b9827c9c47e7b3317
shashi.gharti@gmail.com <shashi.gharti@gmail.com>
2020-02-01 21:16:38 +0545

    fixed compare properties

--- packages/robust/core/resources/views/website/user-profile/partials/favourites.blade.php
+++ packages/robust/core/resources/views/website/user-profile/partials/favourites.blade.php
@@ -1,55 +1,103 @@
-<div class="">
+<div class="user-favourite">
     <h3 class="title-more-detail favourite-title">My Favourites Properties (0)</h3>
     <div class="sort-filter">
         <div class="left-align col s8">
-            <span class="btn btn-xs btn-theme compare-favourite">
+            <a
+                class="user-favourite__btns compare-favourite btn btn-theme modal-trigger"
+                data-base-url="{{ route('website.realestate.market.reports.compare') }}"
+                href="#compare-favourite"
+            >
                 Compare Checkmarked Properties
-            </span>
-            <span class="btn btn-xs btn-green map-favourite">
-                 Show Properties on Map
-            </span>
+            </a>
+            <div id="compare-favourite" class="compare-favourite__table modal">
+                <div class="modal-content">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Property Name</th>
+                            <th>System Price</th>
+                            <th>Asking Price</th>
+                            <th>Sold Price</th>
+                            <th>Bedrooms</th>
+                            <th>Bathrooms</th>
+                            <th>Days on MLS</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @if(isset($lead->favourites) && !empty($lead->favourites))
+                            @foreach($lead->favourites as $key => $listing)
+                                <tr data-id="{{ $listing->id }}" class="hide">
+                                    <td>
+                                        {{ $listing->name }}
+                                    </td>
+                                    <td>
+                                       {{ $listing->system_price }}
+                                    </td>
+                                    <td>
+                                        {{ $listing->asking_price }}
+                                    </td>
+                                    <td>
+                                        {{ $listing->sold_price }}
+                                    </td>
+                                    <td>
+                                        {{ $listing->bedrooms }}
+                                    </td>
+                                    <td>
+                                        {{ $listing->baths_full }}
+                                    </td>
+                                    <td>
+                                        {{ $listing->days_on_mls }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
+                </div>
+            </div>
+            <a id="compare-map"
+               class="user-favourite__btns compare-map btn btn-green"
+               data-base-url="{{ route('website.realestate.market.reports.map') }}"
+            >
+                Show On Map
+            </a>
         </div>
         <div class="right-align">
             <div class="sort-favourite">
-                Sort By- <button class="btn btn-xs btn-default">$High-Low</button> <button class="btn btn-xs btn-default">$Low-High</button>
+                Sort By-
+                <button class="btn btn-xs btn-default">$High-Low</button>
+                <button class="btn btn-xs btn-default">$Low-High</button>
             </div>
         </div>
     </div>
-    <div class="favourite__table col s12 mr-t-20">
+    <div class="user-favourite__table col s12 mr-t-20">
         <table class="striped">
             <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Property Type</th>
-                    <th>Asking Price</th>
-                </tr>
+            <tr>
+                <th>Name</th>
+                <th>Property Type</th>
+                <th>Asking Price</th>
+            </tr>
             </thead>
 
             <tbody>
-                @if(isset($lead->favourites) && !empty($lead->favourites))
-                    @foreach($lead->favourites as $key => $listing)
-                        <tr data-id="{{ $listing->id }}">
-                            <td>
-                                <label>
-                                    {{ Form::checkbox("listing", $listing->slug) }}
-                                    <span>{{ $listing->name }}</span>
-                                </label>
-                            </td>
-                            <td>{{ $listing->class }}</td>
-                            <td>{{ $listing->system_price }}</td>
-                        </tr>
-                        <tr data-id="{{ $listing->id }}">
-                            <td>
-                                <label>
-                                    {{ Form::checkbox("listing", $listing->slug) }}
-                                    <span>{{ $listing->name }}</span>
-                                </label>
-                            </td>
-                            <td>{{ $listing->class }}</td>
-                            <td>{{ $listing->system_price }}</td>
-                        </tr>
-                    @endforeach
-                @endif
+            @if(isset($lead->favourites) && !empty($lead->favourites))
+                @foreach($lead->favourites as $key => $listing)
+                    <tr>
+                        <td>
+                            <label>
+                                {{ Form::checkbox("listings[]", $listing->id ) }}
+                                <span>{{ $listing->name }}</span>
+                            </label>
+                        </td>
+                        <td>{{ $listing->class }}</td>
+                        <td>{{ $listing->system_price }}</td>
+                    </tr>
+                @endforeach
+            @endif
             </tbody>
         </table>
     </div>
--- packages/robust/real-estate/public/website/js/modules/user-profile.js
+++ packages/robust/real-estate/public/website/js/modules/user-profile.js
@@ -1,10 +1,22 @@
 ;
 (function ($, FRW, window, document, undefined) {
     "use strict"
-    $(function () {
-        $('.favourite__table').on('click', function(){
 
+    function resetCompareTable() {
+        let elems = $(".user-favourite__table :input[name='listings[]']");
+        $.each(elems, function (index, elem) {
+            if ($(elem).prop("checked") == true) {
+                $('.compare-favourite__table [data-id=' + $(elem).val() + ']').removeClass('hide');
+            } else {
+                $('.compare-favourite__table [data-id=' + $(elem).val() + ']').addClass('hide');
+            }
         });
+    }
 
+    $(function () {
+        let url = $('.user-favourite #compare-favourite').data('base-url');
+        $(".user-favourite__table :input[name='listings[]']").on('click', function (e) {
+            resetCompareTable();
+        });
     });
 }(jQuery, FRW, window, document));
