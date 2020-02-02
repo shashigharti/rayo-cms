0e0fa0b7bc155192c8fdad93aaec32fc5d6ffe2d
Kundan Karna <mysoulsource.np@gmail.com>
2020-02-02 17:31:49 +0545

    listing alert to leads

--- packages/robust/real-estate/resources/views/admin/email-templates/partials/lead-emails-listing.blade.php
+++ packages/robust/real-estate/resources/views/admin/email-templates/partials/lead-emails-listing.blade.php
@@ -1,9 +1,3 @@
 <p style="padding: 10px 20px;">
-    <span style="font-size: 15px;">Hi *|AGENT_FIRSTNAME|*</span>
-</p>
-<p>*|LEAD_FULLNAME|*</p>
-<p>Phone:&nbsp;*|LEAD_PHONE|*&nbsp;&nbsp;<strong>(Landline)</strong></p>
-<p>Email:&nbsp;*|LEAD_MAIL|*</p>
-<p style="padding: 0px 20px;">sent an email to a friend showing a property at (listing address)<br>
-    it may be in a good time to take a look at (Lead first name)'s favourites and specifics of this property!
+    <span style="font-size: 15px;">Hi *|LEAD_FULLNAME|*</span>
 </p>
