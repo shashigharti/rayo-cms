807bbc34a2f50a37eb78797838507dfb3bbe30f3
Kundan Karna <mysoulsource.np@gmail.com>
2020-02-03 08:53:58 +0545

    added communication event

--- packages/robust/real-estate/src/Listeners/ListingAlertEventListener.php
+++ packages/robust/real-estate/src/Listeners/ListingAlertEventListener.php
@@ -6,6 +6,7 @@ namespace Robust\RealEstate\Listeners;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
+use Robust\RealEstate\Events\LeadCommunicationsEvent;
 use Robust\RealEstate\Events\ListingAlertEvent;
 use Robust\RealEstate\Mail\SendListingAlertEmail;
 
@@ -32,6 +33,8 @@ class ListingAlertEventListener
         $body = replace_variables($template->body, $event->lead, $data);
         $body = replace_listings($body,$event->listings);
         $email = $event->lead->user->email;
+        $agent =  $event->lead->agent_id ?? \Auth::user()->memberable()->first()->id;
+        event(new LeadCommunicationsEvent($agent,$event->lead->id,$data['subject'],$body));
         try {
             Mail::to($email)->send(new SendListingAlertEmail($data['subject'],$body));
         }catch (\Exception $e)
