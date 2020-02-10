216b714a1f9cc49ddd8e56c7ff654e9192a0b70a
Kundan Karna <mysoulsource.np@gmail.com>
2020-02-10 08:48:08 +0545

    remove commented code

--- packages/robust/core/resources/views/website/layouts/default.blade.php
+++ packages/robust/core/resources/views/website/layouts/default.blade.php
@@ -16,21 +16,6 @@
         @foreach($favicons as $icon)
             <link rel="{{$icon['rel'] ?? ''}}" sizes="{{$icon['size'] ?? ''}}" href="{{$icon['href'] ? getMedia($icon['href']) : ''}}">
         @endforeach
-        //remove below code after confirmation
-        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/apple-touch-icon-114x114.png') }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/apple-touch-icon-72x72.png') }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/apple-touch-icon-144x144.png') }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/apple-touch-icon-60x60.png') }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/apple-touch-icon-120x120.png') }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/apple-touch-icon-76x76.png') }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/apple-touch-icon-152x152.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon-180x180.png') }}">
-        <link rel="icon" type="image/png" href="{{ asset('/favicon-192x192.png') }}" sizes="192x192">
-        <link rel="icon" type="image/png" href="{{ asset('/favicon-160x160.png') }}" sizes="160x160">
-        <link rel="icon" type="image/png" href="{{ asset('/favicon-96x96.png') }}" sizes="96x96">
-        <link rel="icon" type="image/png" href="{{ asset('/favicon-16x16.png') }}" sizes="16x16">
-        <link rel="icon" type="image/png" href="{{ asset('/favicon-32x32.png') }}" sizes="32x32">
-        {{--END FAVICONS--}}
 
         <script type='application/ld+json'>
             {
