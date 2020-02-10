5d0ca8ea42560da13a2e0da40ba14356fc9593c9
Kundan Karna <mysoulsource.np@gmail.com>
2020-02-10 09:28:44 +0545

    market report btns

--- packages/robust/core/resources/views/website/market-report/compare.blade.php
+++ packages/robust/core/resources/views/website/market-report/compare.blade.php
@@ -16,8 +16,37 @@
     </header>
 @endsection
 @section('body_section')
-    <section class="market compare-locations main-content" data-page='{{$page_type}}'> 
+    <section class="market compare-locations main-content" data-page='{{$page_type}}'>
+        <div class="container report">
+            <h3 class="txt-title js-bookmark_title"><span>{{settings('general-setting','company_name') ?? ''}} Real Estate Reports</span>
+            </h3>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="inner--title text-center">
+                        <h1>Market Reports</h1>
+                        <p>Serious about real state? Size up the market like a realtor using up to date MLS data!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <p class="text-center">
+                        <b>Sellers -</b>
+                        Research your neighborhood to list your home for the right price. &nbsp; <b>Buyers -</b>Research all neighborhoods in your price range
+                    </p>
+                </div>
+            </div>
+        </div>
         <div class="container-fluid">
+            <div class="row compare_selected_locations_buttons">
+                <a href="#" class="btn btn-success  not-logged-in-btn " style="margin-right:10px"><span class="glyphicon glyphicon-floppy-save"></span> Save this Report</a>
+                <a href="#" class="btn btn-success print-this-page" style="margin-right:10px"><span class="glyphicon glyphicon glyphicon-print"></span> Print</a>
+                <a href="#" class="btn btn-success  not-logged-in-btn " style="margin-right:10px"><span class="glyphicon glyphicon-envelope"></span> Email Report to Friend</a>
+                <a href="#" class="btn btn-success  not-logged-in-btn " style="margin-right:10px"><span class="glyphicon glyphicon-user"></span> Discuss with Realtor</a>
+                <a href="#" title="" class="btn btn-xs btn-success pull-right  not-logged-in-btn " style="margin-right:10px">
+                    Send me prop Alerts
+                </a>
+            </div>
             <div class="row">
                 <div class="col s12">
                     <div class="compare-locations__stat">
